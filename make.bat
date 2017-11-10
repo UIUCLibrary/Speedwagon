ebc7214c8121b30b46c72ceee2774c2761e009cc
hborcher <hborcher@illinois.edu>
2017-11-10 14:27:04 -0600

    cleaned up

--- make.bat
+++ make.bat
@@ -3,18 +3,17 @@
 for /f "tokens=1,* delims= " %%a in ("%*") do set EXTRA_ARGS=%%b
 
 if [%1] == []                call:main                  && goto:eof
-if "%~1" == "install-dev"    call:install-dev           && goto:eof
-if "%~1" == "docs"           call:docs %EXTRA_ARGS%     && goto:eof
-if "%~1" == "venv"           call:venv                  && goto:eof
-if "%~1" == "venvclean"      call:venvclean             && goto:eof
-if "%~1" == "test"           call:test                  && goto:eof
-if "%~1" == "test-mypy"      call:mypy %EXTRA_ARGS%     && goto:eof
 if "%~1" == "build"          call:build                 && goto:eof
 if "%~1" == "gui"            call:gui                   && goto:eof
-if "%~1" == "wheel"          call:wheel                 && goto:eof
 if "%~1" == "sdist"          call:sdist                 && goto:eof
+if "%~1" == "wheel"          call:wheel                 && goto:eof
 if "%~1" == "standalone"     call:standalone            && goto:eof
+if "%~1" == "docs"           call:docs %EXTRA_ARGS%     && goto:eof
 if "%~1" == "clean"          call:clean                 && goto:eof
+if "%~1" == "test"           call:test                  && goto:eof
+if "%~1" == "test-mypy"      call:mypy %EXTRA_ARGS%     && goto:eof
+if "%~1" == "venv"           call:venv                  && goto:eof
+if "%~1" == "venvclean"      call:venvclean             && goto:eof
 if "%~1" == "help"           call:help                  && goto:eof
 if not %ERRORLEVEL% == 0 exit /b %ERRORLEVEL%
 goto :error %*
@@ -26,131 +25,169 @@ EXIT /B 0
 ::=============================================================================
 :help
     echo Available options:
-    echo    make install-dev        Installs the development requirements into active python environment
-    echo    make venv               Creates a virtualenv with development requirements
-    echo    make venvclean          Removes the generated virtualenv
     echo    make build              Creates a build in the build directory
+    echo    make sdist              Build a Python source distribution
+    echo    make wheel              Build a Python built distribution wheel
+    echo    make standalone         Build a standalone distribution
     echo    make docs               Generates html documentation into the docs/build/html directory
+    echo    make clean              Removes generated files
     echo    make test               Runs tests
     echo    make test-mypy          Runs MyPy tests
-    echo    make wheel              Build a Python built distribution wheel
-    echo    make sdist              Build a Python source distribution
-    echo    make standalone         Build a standalone distribution
-    echo    make clean              Removes generated files
-goto :eof
+    echo    make venv               Creates a virtualenv with development requirements
+    echo    make venvclean          Removes the generated virtualenv
+goto:eof
 
 
 ::=============================================================================
 :: Default target if no options are selected
 ::=============================================================================
 :main
-    call:sdist
-    call:wheel
-    call:docs --build-dir dist/docs
-goto :eof
+    call:_venv
+    call:_install-required-deps
+    call:_install-dev
+    call:_update_gui
+    call:_build
+    call:_sdist
+    call:_wheel
+    call:_docs --build-dir dist/docs
+goto:eof
 
 
 ::=============================================================================
 :: Install runtime requirements
 ::=============================================================================
-:install-required-deps
+:_install-required-deps
     setlocal
     echo Syncing runtime dependencies
-    call venv\Scripts\activate.bat && pip install -r requirements.txt --upgrade-strategy only-if-needed -q
+    call venv\Scripts\activate.bat && pip install -r requirements.txt
     endlocal
 goto:eof
+
 ::=============================================================================
 :: Install development requirements
 ::=============================================================================
+
 :install-dev
-    call:venv
-    call:install-required-deps
+    call:_venv
+    call:_install-required-deps
+    call:_install-dev
+goto:eof
+
+
+:_install-dev
     setlocal
     echo Syncing development dependencies
-    call venv\Scripts\activate.bat && pip install -r requirements-dev.txt --upgrade-strategy only-if-needed -q
+    call venv\Scripts\activate.bat && pip install -r requirements-dev.txt
     endlocal
-goto :eof
+goto:eof
 
 
 ::=============================================================================
 :: Build a virtualenv sandbox for development
 ::=============================================================================
 :venv
-    if exist "venv" echo %CD%\venv folder already exists. To activate virtualenv, use venv\Scripts\activate.bat & goto :eof
+    if not exist "venv" call:_venv
+    call :_install-required-deps
+    call:_install-dev
+goto:eof
+
+:_venv
+    if exist "venv" echo Found Python virtualenv && goto:eof
 
     echo Creating a local virtualenv in %CD%\venv
     setlocal
-
     REM Create a new virtualenv in the venv path
     py -m venv venv
-    call :install-required-deps
-
     endlocal
-goto :eof
-
+goto:eof
 
 ::=============================================================================
 :: Remove virtualenv sandbox
 ::=============================================================================
 :venvclean
     if exist "venv" echo removing venv & RD /S /Q venv
-goto :eof
+goto:eof
 
 
 ::=============================================================================
 :: Build the target
 ::=============================================================================
 :build
-    call:install-dev
-    setlocal
-    call venv\Scripts\activate.bat && python setup.py build
-    endlocal
-goto :eof
+    call:_venv
+    call:_install-required-deps
+    call:_install-dev
+    call:_update_gui
+    call:_build
+goto:eof
 
+:_build
+    setlocal
+    call venv\Scripts\activate.bat && python setup.py --quiet build
+    endlocal
+goto:eof
 
 ::=============================================================================
 :: Generate Python GUI files from Qt .ui file located in the ui directory
 ::=============================================================================
 :gui
-    call:install-dev
-    setlocal
-    echo Converting Qt5 .ui files located in the ./ui path into .py files:
-    for %%f in (
-     ui/*.ui ) do (
-        echo     %%~nf
-        pyuic5 ui\%%f -o frames\ui\%%~nf.py
+    call:_install-dev
+    call:_update_gui
+goto:eof
 
-    )
+:_update_gui
+    setlocal
+    call venv\Scripts\activate.bat && python setup.py --quiet build_ui
     endlocal
-goto :eof
+goto:eof
 
 
 ::=============================================================================
 :: Create a wheel distribution
 ::=============================================================================
 :wheel
-    call:install-dev
-    setlocal
-    call venv\Scripts\activate.bat && python setup.py bdist_wheel
-    endlocal
-goto :eof
+    call:_venv
+    call:_install-required-deps
+    call:_install-dev
+    call:_wheel
 
+goto:eof
+
+:_wheel
+    echo Creating a Wheel distribution
+    setlocal
+    call venv\Scripts\activate.bat && python setup.py --quiet bdist_wheel
+    endlocal
+goto:eof
 
 ::=============================================================================
 :: Create a source distribution
 ::=============================================================================
 :sdist
-    call:install-dev
+    call:_venv
+    call:_install-required-deps
+    call:_install-dev
+    call:_update_gui
+    call:_sdist
+
+goto:eof
+
+:_sdist
+    echo Creating a source distribution
     setlocal
-    call venv\Scripts\activate.bat && python setup.py sdist
+    call venv\Scripts\activate.bat && python setup.py --quiet sdist
     endlocal
-goto :eof
+goto:eof
+
 ::=============================================================================
 :: Create a standalone distribution
 ::=============================================================================
 :standalone
-    setlocal
-    call:install-dev
+    call:_venv
+    call:_install-required-deps
+    call:_install-dev
+    REM call:_update_gui
+    REM call:_build
+    echo Creating a standalone distribution
     setlocal
     call venv\Scripts\activate.bat
     call windows_build\build_release.bat
@@ -161,32 +198,45 @@ goto:eof
 :: Run unit tests
 ::=============================================================================
 :test
-    call:install-dev
+    call:_venv
+    call:_install-required-deps
+    call:_install-dev
+    call:_update_gui
     setlocal
     call venv\Scripts\activate.bat && python setup.py test
     endlocal
-goto :eof
+goto:eof
 
 
 ::=============================================================================
 :: Test code with mypy
 ::=============================================================================
 :mypy
-    call:install-dev
+    call:_venv
+    call:_install-required-deps
+    call:_install-dev
     setlocal
     call venv\Scripts\activate.bat && mypy -p frames %*
     endlocal
-goto :eof
+goto:eof
+
+
 ::=============================================================================
 :: Build html documentation
 ::=============================================================================
 :docs
-    call:install-dev
+    call:_venv
+    call:_install-dev
+    call:_build
+    call:_docs %*
+goto:eof
+
+:_docs
     echo Creating docs
     setlocal
     call venv\Scripts\activate.bat && python setup.py build_sphinx %*
     endlocal
-goto :eof
+goto:eof
 
 ::=============================================================================
 :: Clean up any generated files
@@ -196,7 +246,7 @@ goto :eof
 	call venv\Scripts\activate.bat
 
 	echo Calling setup.py clean
-	python setup.py clean --all --quiet
+	python setup.py clean --all
 
     call windows_build/clean_release.bat
 
@@ -212,7 +262,7 @@ goto :eof
 	if exist .tox rd /q /s .tox                     && echo Removed .tox
 	if exist frames.egg-info rd /q /s frames.egg-info && echo Removed frames.egg-info
 	endlocal
-goto :eof
+goto:eof
 
 
 ::=============================================================================
@@ -221,4 +271,4 @@ goto :eof
 :error
     echo Unknown option: %*
     call :help
-goto :eof
+goto:eof
