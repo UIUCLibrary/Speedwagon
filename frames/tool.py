118a0f588200ce0752cada46593f33fb2e3d6603
hborcher <hborcher@illinois.edu>
2017-11-10 14:24:32 -0600

    init commit

---
+++ frames/tool.py
@@ -0,0 +1,20 @@
+import abc
+
+
+class AbsTool(metaclass=abc.ABCMeta):
+    name = None  # type: str
+    description = None  # type: str
+    options = []
+
+
+class MakeChecksumBatch(AbsTool):
+    name = "Make Checksum Batch"
+    description = "Makes a checksums"
+    options = [
+        ("input",),
+        ("output",)
+    ]
+
+class Foo(AbsTool):
+    name = "Foo"
+    description = "foo boo"
