0507053c119a42627e1020053ca81d93ff6d71ff
hborcher <hborcher@illinois.edu>
2017-12-13 15:00:31 -0600

    added tools

---
+++ tests/test_tools.py
@@ -0,0 +1,11 @@
+from frames import tool
+from PyQt5 import QtCore
+import pytest
+
+
+def test_load_tools():
+    tools = tool.available_tools()
+    assert isinstance(tools, dict)
+    assert "Make Checksum Batch" in tools
+    tool_list = tool.ToolsListModel(tools)
+    print(tool_list)
