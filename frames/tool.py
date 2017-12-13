318565730fa0a741599268d3d398473931e1aada
hborcher <hborcher@illinois.edu>
2017-12-13 13:58:51 -0600

    all tools are now in own package

--- frames/tool.py
+++ frames/tool.py
@@ -1,38 +1,52 @@
 import abc
-
+import inspect
+import typing
+from collections import namedtuple
+from . import tools
+from frames.tools.abstool import AbsTool
 import os
-from PyQt5 import QtWidgets
+import random
+import time
+from PyQt5 import QtWidgets, QtCore, QtGui
+from frames.worker import ProcessJob
 
-
-class AbsTool(metaclass=abc.ABCMeta):
-    name = None  # type: str
-    description = None  # type: str
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.options = []  # type: ignore
-
+OptionPair = namedtuple("OptionPair", ("label", "data"))
 
 class AbsToolData(metaclass=abc.ABCMeta):
-    @property
+
+    def __init__(self, parent=None):
+        self._parent = parent
+        self.label = ""
+        self.widget = self.get_widget()
+
     @abc.abstractmethod
-    def widget(self):
+    def get_widget(self):
         pass
 
-    def __init__(self):
-        self.label = ""
+    @property
+    def data(self):
+        return self.widget.value
+class SelectDirectory(AbsToolData):
 
+    def get_widget(self):
+        # return PathSelector2()
+        return PathSelector()
+
+class PathSelector2:
+    def __init__(self, parent=None):
+        self.parent = parent
 
 class PathSelector(QtWidgets.QWidget):
 
     def __init__(self, parent=None, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
+        # self._parent = parent
         layout = QtWidgets.QHBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
         self._value = ""
-        self.line = QtWidgets.QLineEdit()
+        self.line = QtWidgets.QLineEdit(parent=self)
         self.line.editingFinished.connect(self._update_value)
-        self.button = QtWidgets.QPushButton()
+        self.button = QtWidgets.QPushButton(parent=self)
         self.button.setText("Browse")
         self.button.clicked.connect(self.get_path)
         layout.addWidget(self.line)
@@ -49,7 +63,7 @@ class PathSelector(QtWidgets.QWidget):
             self.value = path
 
     def _update_value(self):
-        print("Value is {}".format(self.line.text()))
+        print("Value is {}".format(self.value))
 
     @staticmethod
     def _is_valid(value):
@@ -58,73 +72,126 @@ class PathSelector(QtWidgets.QWidget):
 
     @property
     def value(self):
-        return self._value
+        return self.line.text()
 
     @value.setter
     def value(self, value):
-        self._value = value
+        print("My value is now {}".format(value))
+        # self._value = value
         self.line.setText(value)
 
-
-class SelectDirectory(AbsToolData):
-    @property
-    def widget(self):
-        return PathSelector()
+    def destroy(self, destroyWindow=True, destroySubWindows=True):
+        print("destroyed")
+        super().destroy(destroyWindow, destroySubWindows)
 
 
-class MakeChecksumBatch(AbsTool):
-    name = "Make Checksum Batch"
-    description = "Makes a checksums"
+class ToolsListModel(QtCore.QAbstractTableModel):
 
-    def __init__(self) -> None:
-        super().__init__()
-        source = SelectDirectory()
-        source.label = "Source"
-        self.options.append(source)
+    NAME = 0
+    DESCRIPTION = 1
+
+    def __init__(self, data: typing.Dict["str", AbsTool], parent=None):
+        super().__init__(parent)
+        self._data = []
+        for k, v in data.items():
+            self._data.append(v)
+
+    def rowCount(self, parent=None, *args, **kwargs):
+        return len(self._data)
+
+    def data(self, index, role=None):
+        if index.isValid():
+            data = self._data[index.row()]
+            if role == QtCore.Qt.DisplayRole:
+                if index.column() == ToolsListModel.NAME:
+                    return data.name.title()
+                if index.column() == ToolsListModel.DESCRIPTION:
+                    return data.description
+                else:
+                    print(index.column())
+            if role == QtCore.Qt.UserRole:
+                return self._data[index.row()]
+
+        return QtCore.QVariant()
+
+    def flags(self, index):
+        # if index.isValid():
+        return super().flags(index)
+
+    def setData(self, QModelIndex, Any, role=None):
+        return super().setData(QModelIndex, Any, role)
+
+    def columnCount(self, parent=QtCore.QModelIndex(), *args, **kwargs):
+        return 2
 
 
-class Spam(AbsTool):
-    name = "Spam"
-    description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac diam id purus pretium " \
-                  "venenatis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia " \
-                  "Curae; Fusce laoreet fermentum lorem et pretium. Duis iaculis venenatis sagittis. Nulla tristique " \
-                  "tellus at dolor laoreet maximus. Aenean ac posuere augue, quis volutpat felis. Phasellus egestas " \
-                  "orci id erat fringilla, in euismod purus luctus. Proin faucibus condimentum imperdiet. Lorem " \
-                  "ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor eu erat at congue. " \
-                  "Quisque feugiat pulvinar eleifend. Nulla tincidunt nibh velit, non fermentum lorem pharetra at. " \
-                  "Sed eleifend sapien ut faucibus convallis. Orci varius natoque penatibus et magnis dis parturient " \
-                  "montes, nascetur ridiculus mus. Nullam lacinia sed augue quis iaculis. Aliquam commodo dictum mi, " \
-                  "non semper quam varius ut."
+class ToolOptionsModule(QtCore.QAbstractTableModel):
 
-    def __init__(self) -> None:
-        super().__init__()
-
-        input_data = SelectDirectory()
-        input_data.label = "Input"
-        self.options.append(input_data)
-
-        output_data = SelectDirectory()
-        output_data.label = "Output"
-        self.options.append(output_data)
+    def __init__(self, data: dict, parent=None):
+        super().__init__(parent)
+        self._data = dict()
+        for i, (k, v) in enumerate(data.items()):
+            self._data[i] = OptionPair(k,v)
 
 
-class Eggs(AbsTool):
-    name = "Eggs"
-    description = "Sed odio sem, vestibulum a lacus sed, posuere porta neque. Ut urna arcu, dignissim a dolor ac, " \
-                  "sollicitudin pellentesque mi. Curabitur feugiat interdum mauris nec venenatis. In arcu elit, " \
-                  "scelerisque et bibendum id, faucibus id enim. Proin dui mi, imperdiet eget varius ut, faucibus at " \
-                  "lectus. Sed accumsan quis turpis id bibendum. Mauris in ligula nec tortor vulputate vulputate. " \
-                  "Nullam tincidunt leo nec odio tincidunt malesuada. Integer ut massa dictum, scelerisque turpis " \
-                  "eget, auctor nibh. Vestibulum sollicitudin sem eget enim congue tristique. Cras sed purus ac diam " \
-                  "pulvinar scelerisque et efficitur justo. Duis eu nunc arcu"
+    def rowCount(self, parent=None, *args, **kwargs):
+        return len(self._data)
 
-    def __init__(self) -> None:
-        super().__init__()
+    def columnCount(self, parent=None, *args, **kwargs):
+        if len(self._data) > 0:
+            return 1
+        else:
+            return 0
 
-        input_data = SelectDirectory()
-        input_data.label = "Goes In"
-        self.options.append(input_data)
+    def data(self, index, role=None):
+        if index.isValid():
+            if role == QtCore.Qt.DisplayRole:
+                return self._data[index.row()].data
+            if role == QtCore.Qt.EditRole:
+                return self._data[index.row()].data
+        return QtCore.QVariant()
 
-        output_data = SelectDirectory()
-        output_data.label = "Goes out"
-        self.options.append(output_data)
+    def flags(self, index):
+        if not index.isValid():
+            return QtCore.Qt.ItemIsEnabled
+
+        column = index.column()
+        if column == 0:
+            return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEditable
+
+    def setData(self, index, data, role=None):
+        if not index.isValid():
+            return False
+        existing_data = self._data[index.row()]
+        # print(data)
+        self._data[index.row()] = OptionPair(existing_data.label, data)
+        # self._data[]
+        return True
+        # return super().setData(QModelIndex, data, role)
+
+    def headerData(self, index, Qt_Orientation, role=None):
+        if Qt_Orientation == QtCore.Qt.Vertical:
+            if role == QtCore.Qt.DisplayRole:
+                title = self._data[index].label
+                return str(title).title()
+        return QtCore.QVariant()
+        # return super().headerData(index, Qt_Orientation, role)
+
+    def get(self) -> dict:
+        options = dict()
+        for i, (k, v) in self._data.items():
+            options[k] = v
+        return options
+
+def available_tools()->dict:
+    """
+    Locate all tools that can be loaded
+
+    Returns: Dictionary of all tools
+
+    """
+    located_tools = dict()
+    for name_, module_class in inspect.getmembers(tools, lambda m: inspect.isclass(m) and not inspect.isabstract(m)):
+        if issubclass(module_class, AbsTool):
+            located_tools[module_class.name] = module_class
+    return located_tools
