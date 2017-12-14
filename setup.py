13f67b48ca43b3524989a369a6d119fe21d579ae
hborcher <hborcher@illinois.edu>
2017-12-14 17:57:28 -0600

    added HathiValidate to requirements

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ except ImportError:
 setup(
     test_suite="tests",
     install_requires=[
-        "pyqt5", "hathizip"
+        "pyqt5", "hathizip", "HathiValidate"
     ],
     packages=[
         "forseti",
