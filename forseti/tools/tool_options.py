7bc8c95e5b167ea552e66a946e9ad6adb5e07ac6
Henry Borchers <henryborchers@yahoo.com>
2017-12-15 17:07:27 -0600

    added data_type

--- forseti/tools/tool_options.py
+++ forseti/tools/tool_options.py
@@ -1,5 +1,6 @@
 class ToolOption:
 
-    def __init__(self, name) -> None:
+    def __init__(self, name, data_type=str) -> None:
         self.name = name
         self.data = ""
+        self.data_type = data_type
