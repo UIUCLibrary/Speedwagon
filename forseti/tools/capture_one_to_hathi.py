1638f24ee9c58bccf4e19679af20b19aef2dd026
hborcher <hborcher@illinois.edu>
2017-12-22 13:22:55 -0600

    Added MeduaPackager

--- forseti/tools/capture_one_to_hathi.py
+++ forseti/tools/capture_one_to_hathi.py
@@ -7,6 +7,9 @@ from forseti.tools.abstool import AbsTool
 from forseti.tools.tool_options import ToolOptionDataType
 from forseti.worker import ProcessJob
 
+import MedusaPackager
+
+
 # TODO: This needs the code from https://github.com/UIUCLibrary/DCCMedusaPackager
 
 class CaptureOneToHathiTiffPackage(AbsTool):
@@ -20,9 +23,26 @@ class CaptureOneToHathiTiffPackage(AbsTool):
                   "filename."
 
     @staticmethod
-    def discover_jobs(**user_args):
+    def discover_jobs(**user_args) -> typing.List[dict]:
         jobs = []
+        source_input = user_args['input']
+        dest = user_args['output']
+        unsorted_data = MedusaPackager.find_package_files(source_input)
+        packages = unsorted_data.split_items(MedusaPackager.dash_grouper)
+        for package in sorted(packages, key=lambda x: x.package_name):
+            data = package.sorted()
+            new_package_path = os.path.join(dest, data.package_name)
+            jobs.append(
+                {
+                    "existing_package": data,
+                    "new_package_root": new_package_path
+                }
+            )
+            # jobs.append(package.sorted())
+            if not os.path.exists(new_package_path):
+                MedusaPackager.create_empty_package(new_package_path)
         # cli.get_packages()
+        # TODO: Fix this so that it only happen once in another method
         return jobs
 
     def new_job(self) -> typing.Type[worker.ProcessJob]:
@@ -47,4 +67,7 @@ class CaptureOneToHathiTiffPackage(AbsTool):
 class PackageConverter(ProcessJob):
 
     def process(self, *args, **kwargs):
-        pass
+        existing_package = kwargs['existing_package']
+        new_package_root = kwargs['new_package_root']
+        # if not os.path.exists()
+        print(f"args = {args} kwargs={kwargs}")
