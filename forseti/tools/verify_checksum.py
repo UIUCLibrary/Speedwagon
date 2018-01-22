e06e4310798687d101f8aba778ad33d549b0a6d9
hborcher <hborcher@illinois.edu>
2018-01-22 13:24:45 -0600

    added period at end of sentence of description

--- forseti/tools/verify_checksum.py
+++ forseti/tools/verify_checksum.py
@@ -51,7 +51,7 @@ class VerifyChecksumBatch(AbsTool):
     name = "Verify Checksum Batch"
     description = "Verify checksum values in checksum batch file, report errors. Input is a text file containing a " \
                   "list of multiple files and their md5 values. The listed files are expected to be siblings to the " \
-                  "checksum file"
+                  "checksum file."
 
     def __init__(self) -> None:
         super().__init__()
