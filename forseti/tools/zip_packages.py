efc1bb9cff3a9ef3447feb1110161af1bab2ea13
hborcher <hborcher@illinois.edu>
2018-01-30 10:30:04 -0600

    pipe the log information to the console

--- forseti/tools/zip_packages.py
+++ forseti/tools/zip_packages.py
@@ -1,24 +1,27 @@
+import logging
 import random
 import typing
 import time
 
 import os
+from contextlib import contextmanager
 
 from forseti import worker
 # from frames.tool import  ZipPackageJob
 from forseti.tools.abstool import AbsTool
 # from forseti.tool import ToolOption
-from forseti.worker import ProcessJob
+from forseti.worker import ProcessJob, GuiLogHandler
 # from .tool_options import UserOption
 from forseti.tools import tool_options
 import hathizip.process
+import hathizip
 
 class ZipPackages(AbsTool):
     name = "Zip packages"
     description = "This tool takes a folder, usually of HathiTrust packages, zips each subfolder, and copies the " \
-                " resultant tree to a different location. Input is a root folder, usually for a HathiTrust shipment, " \
-                " containing multiple subfolders, each one a HathiTrust digitized item." \
-                "\nOutput is a destination location for the newly generated files."
+                  " resultant tree to a different location. Input is a root folder, usually for a HathiTrust shipment, " \
+                  " containing multiple subfolders, each one a HathiTrust digitized item." \
+                  "\nOutput is a destination location for the newly generated files."
 
     def __init__(self) -> None:
 
@@ -33,7 +36,7 @@ class ZipPackages(AbsTool):
         return ZipPackageJob
 
     @staticmethod
-    def discover_jobs(source, output, *args, **kwargs) -> typing.List[dict]:   # type: ignore
+    def discover_jobs(source, output, *args, **kwargs) -> typing.List[dict]:  # type: ignore
         ZipPackages.validate_args(source, output, args, kwargs)
 
         job_requests = []
@@ -65,15 +68,25 @@ class ZipPackages(AbsTool):
             tool_options.UserOptionCustomDataType("output", tool_options.FolderData),
         ]
 
+
 class ZipPackageJob(ProcessJob):
+    @contextmanager
+    def log_config(self, logger):
+        gui_logger = GuiLogHandler(self.log)
+        try:
+            logger.addHandler(gui_logger)
+            yield
+        finally:
+            logger.removeHandler(gui_logger)
 
     def process(self, source_path, destination_path, *args, **kwargs):
-
-        self.log("Zipping {}".format(source_path))
-        hathizip.process.compress_folder(path=source_path, dst=destination_path)
-        # self.log("{} successfully zipped to {}".format(source_path, destination_path))
-        basename = os.path.basename(source_path)
-        newfile = os.path.join(destination_path, f"{basename}.zip")
-        self.log(f"Created {newfile}")
-        self.result = newfile
-
+        my_logger = logging.getLogger(hathizip.__name__)
+        my_logger.setLevel(logging.INFO)
+        with self.log_config(my_logger):
+            self.log("Zipping {}".format(source_path))
+            hathizip.process.compress_folder(path=source_path, dst=destination_path)
+            # self.log("{} successfully zipped to {}".format(source_path, destination_path))
+            basename = os.path.basename(source_path)
+            newfile = os.path.join(destination_path, f"{basename}.zip")
+            self.log(f"Created {newfile}")
+            self.result = newfile
