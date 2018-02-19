f76a2cd0c586cf63267ceeb2d17a289b366affe7
hborcher <hborcher@illinois.edu>
2018-02-19 14:53:30 -0600

    commented out dead code

--- forseti/runner_strategies.py
+++ forseti/runner_strategies.py
@@ -32,92 +32,92 @@ class RunRunner:
         return self._strategy.run(parent, tool, options, on_success, on_failure, logger)
 
 
-class UsingWorkWrapper(AbsRunner):
+# class UsingWorkWrapper(AbsRunner):
+#
+#     def __init__(self) -> None:
+#         super().__init__()
+#         warnings.warn("Use UsingWorkManager instead", DeprecationWarning)
+#
+#     def run(self, parent, tool: forseti.tools.abstool.AbsTool, options: dict, on_success, on_failure,
+#             logger: logging.Logger):
+#         with worker.WorkWrapper(parent, tool, logger=logger) as work_manager:
+#
+#             work_manager.worker_display.finished.connect(on_success)
+#             work_manager.worker_display.failed.connect(on_failure)
+#
+#             try:
+#                 # self.log_manager.debug("Validating arguments")
+#                 work_manager.valid_arguments(options)
+#                 # tool.validate_args(**options)
+#                 # wm.completion_callback = lambda: self._tool.on_completion()
+#
+#                 # Search for jobs
+#                 job_searcher = forseti.gui.JobSearcher(tool, options)
+#                 # print("Job search starting", file=sys.stderr)
+#                 job_searcher.start()
+#                 while True:
+#                     if job_searcher.isFinished():
+#                         break
+#                     else:
+#                         time.sleep(0.01)
+#                     # self.log_manager.info("Loading")
+#                     QtCore.QCoreApplication.processEvents()
+#                     # self.QApplication.processEvents()
+#                 # print("Job search Finished", file=sys.stderr)
+#
+#                 for _job_args in job_searcher.jobs:
+#                     # self.log_manager.debug("Adding {} with {} to work manager".format(tool, _job_args))
+#                     work_manager.add_job(_job_args)
+#
+#                 print("running {} tasks".format(work_manager.worker_display._jobs_queue.qsize()), file=sys.stderr)
+#                 try:
+#                     work_manager.run()
+#
+#                     # print("AFTER")
+#                     # work_manager.worker_display.finish()
+#
+#                 except RuntimeError as e:
+#                     QtWidgets.QMessageBox.warning(parent, "Process failed", str(e))
+#                 # except TypeError as e:
+#                 #     QtWidgets.QMessageBox.critical(self, "Process failed", str(e))
+#                 #     raise
+#
+#             except ValueError as e:
+#
+#                 # if work_manager._working is not None:
+#                 #     work_manager.worker_display.cancel(e, quiet=True)
+#                 work_manager.worker_display.cancel(quiet=True)
+#                 QtWidgets.QMessageBox.warning(parent, "Invalid setting", str(e))
+#                 return
+#
+#             except Exception as e:
+#                 work_manager.worker_display.cancel(quiet=True)
+#                 exception_message = traceback.format_exception(type(e), e, tb=e.__traceback__)
+#                 msg = QtWidgets.QMessageBox(self)
+#                 msg.setIcon(QtWidgets.QMessageBox.Critical)
+#                 msg.setWindowTitle(str(type(e).__name__))
+#                 msg.setText(str(e))
+#                 msg.setDetailedText("".join(exception_message))
+#                 # self.log_manager.fatal("Terminating application. Reason: {}".format(e))
+#                 msg.exec_()
+#                 print("Exiting early", file=sys.stderr)
+#                 sys.exit(1)
+#             finally:
+#                 print("out!", file=sys.stderr)
 
-    def __init__(self) -> None:
-        super().__init__()
-        warnings.warn("Use UsingWorkManager instead", DeprecationWarning)
 
-    def run(self, parent, tool: forseti.tools.abstool.AbsTool, options: dict, on_success, on_failure,
-            logger: logging.Logger):
-        with worker.WorkWrapper(parent, tool, logger=logger) as work_manager:
-
-            work_manager.worker_display.finished.connect(on_success)
-            work_manager.worker_display.failed.connect(on_failure)
-
-            try:
-                # self.log_manager.debug("Validating arguments")
-                work_manager.valid_arguments(options)
-                # tool.validate_args(**options)
-                # wm.completion_callback = lambda: self._tool.on_completion()
-
-                # Search for jobs
-                job_searcher = forseti.gui.JobSearcher(tool, options)
-                # print("Job search starting", file=sys.stderr)
-                job_searcher.start()
-                while True:
-                    if job_searcher.isFinished():
-                        break
-                    else:
-                        time.sleep(0.01)
-                    # self.log_manager.info("Loading")
-                    QtCore.QCoreApplication.processEvents()
-                    # self.QApplication.processEvents()
-                # print("Job search Finished", file=sys.stderr)
-
-                for _job_args in job_searcher.jobs:
-                    # self.log_manager.debug("Adding {} with {} to work manager".format(tool, _job_args))
-                    work_manager.add_job(_job_args)
-
-                print("running {} tasks".format(work_manager.worker_display._jobs_queue.qsize()), file=sys.stderr)
-                try:
-                    work_manager.run()
-
-                    # print("AFTER")
-                    # work_manager.worker_display.finish()
-
-                except RuntimeError as e:
-                    QtWidgets.QMessageBox.warning(parent, "Process failed", str(e))
-                # except TypeError as e:
-                #     QtWidgets.QMessageBox.critical(self, "Process failed", str(e))
-                #     raise
-
-            except ValueError as e:
-
-                # if work_manager._working is not None:
-                #     work_manager.worker_display.cancel(e, quiet=True)
-                work_manager.worker_display.cancel(quiet=True)
-                QtWidgets.QMessageBox.warning(parent, "Invalid setting", str(e))
-                return
-
-            except Exception as e:
-                work_manager.worker_display.cancel(quiet=True)
-                exception_message = traceback.format_exception(type(e), e, tb=e.__traceback__)
-                msg = QtWidgets.QMessageBox(self)
-                msg.setIcon(QtWidgets.QMessageBox.Critical)
-                msg.setWindowTitle(str(type(e).__name__))
-                msg.setText(str(e))
-                msg.setDetailedText("".join(exception_message))
-                # self.log_manager.fatal("Terminating application. Reason: {}".format(e))
-                msg.exec_()
-                print("Exiting early", file=sys.stderr)
-                sys.exit(1)
-            finally:
-                print("out!", file=sys.stderr)
-
-
-class UsingWorkManager(AbsRunner):
-    def run(self, parent, tool: forseti.tools.abstool.AbsTool, options: dict, on_success, on_failure, logger):
-        worker_manager = worker.WorkerManager(title=tool.name, tool=tool, parent=parent, logger=logger)
-        # worker_manager.logger = logger
-        try:
-            with worker_manager.open(options) as work_runner:
-                work_runner.start()
-                work_runner.finish()
-
-            on_success(worker_manager.results, tool.on_completion)
-        except Exception as e:
-            on_failure(e)
+# class UsingWorkManager(AbsRunner):
+#     def run(self, parent, tool: forseti.tools.abstool.AbsTool, options: dict, on_success, on_failure, logger):
+#         worker_manager = worker.WorkerManager(title=tool.name, tool=tool, parent=parent, logger=logger)
+#         # worker_manager.logger = logger
+#         try:
+#             with worker_manager.open(options) as work_runner:
+#                 work_runner.start()
+#                 work_runner.finish()
+#
+#             on_success(worker_manager.results, tool.on_completion)
+#         except Exception as e:
+#             on_failure(e)
 
 
 class UsingExternalManager(AbsRunner):
@@ -143,13 +143,18 @@ class UsingExternalManager(AbsRunner):
                 i = -1
                 for i, new_setting in enumerate(tool.discover_jobs(**options)):
                     self._manager.add_job(tool, new_setting)
-                    logger.info("Found {} jobs".format(i + 1))
+                logger.info("Found {} jobs".format(i + 1))
                 runner.dialog.setMaximum(i)
 
                 self._manager.start()
 
                 runner.dialog.show()
-                results = self._manager.get_results(update_progress)
+
+                results = list()
+                for result in self._manager.get_results(update_progress):
+                    # print(result)
+                    results.append(result)
+                logger.removeHandler(runner.progress_dialog_box_handler)
 
                 on_success(results, tool.on_completion)
         except Exception as e:
