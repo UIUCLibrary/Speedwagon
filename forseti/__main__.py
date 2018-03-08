bc8a7738ff1c0d02049acfab59db060f33ae3fca
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-03-08 12:44:14 -0600

    simplified the naming

--- forseti/__main__.py
+++ forseti/__main__.py
@@ -1,23 +1,20 @@
 import logging
 import sys
 import pkg_resources
-from forseti import gui
+import forseti
+import forseti.gui
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler())
 
 def main():
-    try:
-        dist = pkg_resources.get_distribution("forseti")
-        print("{}: {}".format(dist.project_name, dist.version))
-    except pkg_resources.DistributionNotFound:
-        print("Development version")
+    print("{}: {}".format(forseti.__name__, forseti.__version__))
 
     if len(sys.argv) > 1 and sys.argv[1] == "--pytest":
         import pytest  # type: ignore
         sys.exit(pytest.main(sys.argv[2:]))
     else:
-        gui.main()
+        forseti.gui.main()
 
 
 if __name__ == '__main__':
