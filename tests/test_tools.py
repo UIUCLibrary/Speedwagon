80f5575992bcee14586b20c4b819cb266f4e4660
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-03-09 16:16:30 -0600

    updated import location

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,3 +1,4 @@
+import forseti.models
 from forseti import tool
 # from PyQt5 import QtCore
 import pytest
@@ -7,5 +8,5 @@ def test_load_tools():
     tools = tool.available_tools()
     assert isinstance(tools, dict)
     assert "Make Checksum Batch [Single]" in tools
-    tool_list = tool.ToolsListModel(tools)
+    tool_list = forseti.models.ToolsListModel(tools)
     print(tool_list)
