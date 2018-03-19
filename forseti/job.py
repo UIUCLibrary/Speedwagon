2bfaed5739a51c167477d5d841f9614840337afc
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-03-19 16:43:58 -0500

    moved contents of tool.py and workflow.py into job.py

--- forseti/job.py
+++ forseti/job.py
@@ -1,4 +1,8 @@
 import abc
+import importlib
+import inspect
+import os
+import sys
 import typing
 import forseti.tasks
 import forseti.tools.options
@@ -80,16 +84,136 @@ class AbsWorkflow(AbsJob):
     def discover_task_metadata(self, **user_args) -> typing.List[dict]:
         pass
 
-    def completion_task(self, task_builder: forseti.tasks.TaskBuilder, results, **user_args) -> None:
+    def completion_task(self, task_builder: "forseti.tasks.TaskBuilder", results, **user_args) -> None:
         pass
 
-    def initial_task(self, task_builder: forseti.tasks.TaskBuilder, **user_args) -> None:
+    def initial_task(self, task_builder: "forseti.tasks.TaskBuilder", **user_args) -> None:
         pass
 
     @classmethod
-    def generate_report(cls, results: typing.List[forseti.tasks.Result], **user_args) -> typing.Optional[str]:
+    def generate_report(cls, results: typing.List["forseti.tasks.Result"], **user_args) -> typing.Optional[str]:
         pass
 
     # @abc.abstractmethod
     # def user_options(self):
     #     return {}
+
+
+class AbsDynamicFinder(metaclass=abc.ABCMeta):
+
+    def __init__(self, path) -> None:
+        self.path = path
+
+    @staticmethod
+    @abc.abstractmethod
+    def py_module_filter(item: os.DirEntry) -> bool:
+        pass
+
+    def locate(self) -> typing.Dict["str", AbsJob]:
+        located_class = dict()
+        tree = os.scandir(self.path)
+
+        for m in filter(self.py_module_filter, tree):
+            for name, module in self.load(m.name):
+                located_class[name] = module
+        return located_class
+
+    @property
+    @abc.abstractmethod
+    def base_class(self) -> typing.Type[AbsJob]:
+        pass
+
+    def load(self, module_file) -> typing.Iterable[typing.Tuple[str, typing.Any]]:
+        def class_member_filter(item):
+            return inspect.isclass(item) and not inspect.isabstract(item)
+
+        try:
+            module = importlib.import_module("{}.{}".format(self.package_name, os.path.splitext(module_file)[0]))
+            members = inspect.getmembers(module, class_member_filter)
+
+            for name_, module_class in members:
+                if issubclass(module_class, self.base_class) and module_class.active:
+                    yield module_class.name, module_class
+
+        except ImportError as e:
+            print("Unable to load {}. Reason: {}".format(module_file, e), file=sys.stderr)
+
+    @property
+    @abc.abstractmethod
+    def package_name(self) -> str:
+        pass
+
+
+class AbsToolData(metaclass=abc.ABCMeta):
+
+    def __init__(self, parent=None):
+        self._parent = parent
+        self.label = ""
+        self.widget = self.get_widget()
+
+    @abc.abstractmethod
+    def get_widget(self):
+        pass
+
+    @property
+    def data(self):
+        return self.widget.value
+
+
+class ToolFinder(AbsDynamicFinder):
+
+    @staticmethod
+    def py_module_filter(item: os.DirEntry):
+        if not str(item.name).startswith("tool_"):
+            return False
+        return True
+
+    @property
+    def package_name(self) -> str:
+        return "{}.tools".format(__package__)
+
+    @property
+    def base_class(self):
+        return AbsTool
+
+
+def available_tools() -> dict:
+    """
+    Locate all tools that can be loaded
+
+    Returns: Dictionary of all tools
+
+    """
+    root = os.path.join(os.path.dirname(__file__), "tools")
+    finder = ToolFinder(root)
+    return finder.locate()
+
+
+class WorkflowFinder(AbsDynamicFinder):
+
+    @staticmethod
+    def py_module_filter(item: os.DirEntry) -> bool:
+        if not str(item.name).startswith("workflow_"):
+            return False
+        return True
+
+    @property
+    def package_name(self) -> str:
+        return "{}.workflows".format(__package__)
+
+    @property
+    def base_class(self):
+        return AbsWorkflow
+
+
+def available_workflows() -> dict:
+    """
+    Locate all workflow class found in workflows subpackage with the workflow prefix
+
+    Returns: Dictionary of all workflow
+
+    """
+
+    root = os.path.join(os.path.dirname(__file__), "workflows")
+    finder = WorkflowFinder(root)
+    return finder.locate()
