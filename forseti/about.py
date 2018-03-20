568a4b191c42bf9ce65ee553c9346500f5bb4ce1
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-03-20 12:56:22 -0500

    added summary from the metadata

--- forseti/about.py
+++ forseti/about.py
@@ -1,13 +1,15 @@
 from PyQt5 import QtWidgets
 import forseti
+import email
 
 
 def about_dialog_box(parent):
-    message = f"{forseti.__name__.title()}" \
+    distribution = forseti.get_project_distribution()
+    metadata = dict(email.message_from_string(distribution.get_metadata(distribution.PKG_INFO)))
+    summary = metadata['Summary']
+
+    message = f"{forseti.__name__.title()}: {forseti.__version__}" \
               f"\n" \
               f"\n" \
-              f"Collection of tools and workflows for DS" \
-              f"\n" \
-              f"\n" \
-              f"Version {forseti.__version__}"
+              f"{summary}"
     QtWidgets.QMessageBox.about(parent, "About", message)
