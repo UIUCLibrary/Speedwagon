2e238fd4250b91bc1d32607994cb16e45f51c5c2
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-03-20 13:00:52 -0500

    pep8

--- forseti/about.py
+++ forseti/about.py
@@ -1,15 +1,22 @@
 from PyQt5 import QtWidgets
 import forseti
 import email
+import pkg_resources
 
 
 def about_dialog_box(parent):
-    distribution = forseti.get_project_distribution()
-    metadata = dict(email.message_from_string(distribution.get_metadata(distribution.PKG_INFO)))
-    summary = metadata['Summary']
+    try:
+        distribution = forseti.get_project_distribution()
+        metadata = dict(
+            email.message_from_string(
+                distribution.get_metadata(distribution.PKG_INFO)))
+        summary = metadata['Summary']
+        message = f"{forseti.__name__.title()}: {forseti.__version__}" \
+                  f"\n" \
+                  f"\n" \
+                  f"{summary}"
 
-    message = f"{forseti.__name__.title()}: {forseti.__version__}" \
-              f"\n" \
-              f"\n" \
-              f"{summary}"
+    except pkg_resources.DistributionNotFound:
+                message = f"{forseti.__name__.title()}: {forseti.__version__}"
+
     QtWidgets.QMessageBox.about(parent, "About", message)
