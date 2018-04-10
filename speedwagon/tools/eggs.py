d53a8bee77c791664ebd6b406a4cb1c2e6a036ce
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-04-10 11:49:22 -0500

    pep8

--- speedwagon/tools/eggs.py
+++ speedwagon/tools/eggs.py
@@ -7,19 +7,25 @@ from speedwagon import worker
 from speedwagon.tools.abstool import AbsTool
 
 from speedwagon.worker import ProcessJobWorker
+
+
 class Eggs(AbsTool):
     name = "Eggs"
-    description = "Sed odio sem, vestibulum a lacus sed, posuere porta neque. Ut urna arcu, dignissim a dolor ac, " \
-                  "sollicitudin pellentesque mi. Curabitur feugiat interdum mauris nec venenatis. In arcu elit, " \
-                  "scelerisque et bibendum id, faucibus id enim. Proin dui mi, imperdiet eget varius ut, faucibus at " \
-                  "lectus. Sed accumsan quis turpis id bibendum. Mauris in ligula nec tortor vulputate vulputate. " \
-                  "Nullam tincidunt leo nec odio tincidunt malesuada. Integer ut massa dictum, scelerisque turpis " \
-                  "eget, auctor nibh. Vestibulum sollicitudin sem eget enim congue tristique. Cras sed purus ac diam " \
-                  "pulvinar scelerisque et efficitur justo. Duis eu nunc arcu"
+    description = "Sed odio sem, vestibulum a lacus sed, posuere porta " \
+                  "neque. Ut urna arcu, dignissim a dolor ac, sollicitudin " \
+                  "pellentesque mi. Curabitur feugiat interdum mauris nec " \
+                  "venenatis. In arcu elit, scelerisque et bibendum id, " \
+                  "faucibus id enim. Proin dui mi, imperdiet eget varius " \
+                  "ut, faucibus at lectus. Sed accumsan quis turpis id " \
+                  "bibendum. Mauris in ligula nec tortor vulputate " \
+                  "vulputate. Nullam tincidunt leo nec odio tincidunt " \
+                  "malesuada. Integer ut massa dictum, scelerisque turpis " \
+                  "eget, auctor nibh. Vestibulum sollicitudin sem eget enim " \
+                  "congue tristique. Cras sed purus ac diam pulvinar " \
+                  "scelerisque et efficitur justo. Duis eu nunc arcu"
 
     def __init__(self) -> None:
         super().__init__()
-
 
     @staticmethod
     def new_job() -> typing.Type[worker.ProcessJobWorker]:
@@ -31,6 +37,7 @@ class Eggs(AbsTool):
         for x in range(10):
             jobs.append({"num": x})
         return jobs
+
 
 class EggsJob(ProcessJobWorker):
     def process(self, num=0, *args):
