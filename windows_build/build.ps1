c190cd28f16f8626ccc429a2b407b145aa53df39
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-05-11 11:45:48 -0500

    init commit

---
+++ windows_build/build.ps1
@@ -0,0 +1,22 @@
+﻿$installationPath = & vswhere.exe -prerelease -latest -property installationPath
+$project_folder = & Get-Location
+
+echo "Visual Studio path = $installationPath"
+echo "Project Folder     = $project_folder"
+
+if ($installationPath -and (test-path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
+  & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -no_logo -host_arch=amd64 && set" | foreach-object {
+    $name, $value = $_ -split '=', 2
+    set-content env:\"$name" $value
+  }
+}
+else
+{
+    echo "Unable to set Visual studio"
+    EXIT 1
+}
+nuget install windows_build\packages.config -OutputDirectory build\nugetpackages
+
+Get-ChildItem Env:
+
+Start-Process -NoNewWindow -FilePath msbuild.exe -ArgumentList "windows_build\release.pyproj /nologo /t:msi /p:ProjectRoot=$project_folder" -Wait
