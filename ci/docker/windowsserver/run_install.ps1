ff64cf8c32cf386e63d9799c3b1f77371e28028f
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-08-17 14:13:35 -0500

    init commit

---
+++ ci/docker/windowsserver/run_install.ps1
@@ -0,0 +1,11 @@
+﻿$MSIFILES = Get-ChildItem -Path . -Filter *.msi
+if($MSIFILES.Count -ne 1){
+    Write-Host "Found more than one msi file. Quiting"
+    exit 1
+}
+
+foreach ($MSIFILE in $MSIFILES) {
+    echo "$MSIFILE"
+    msiexec /i $MSIFILE /q /lp dockerinstall.log
+}
+
