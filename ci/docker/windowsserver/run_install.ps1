07a02c0f66145a9032b98d99ea9c3e42e000bd8f
Henry Borchers <henryborchers@yahoo.com>
2018-08-20 12:02:59 -0500

    use try catch blocks

--- ci/docker/windowsserver/run_install.ps1
+++ ci/docker/windowsserver/run_install.ps1
@@ -1,33 +1,49 @@
 ﻿$MSIFILES = Get-ChildItem -Path . -Filter *.msi
 
 # Only continue if one and only one msi file is located
-
-# If there are no files found, then there was a failure during the building of the docker image
-if($MSIFILES.Count -eq 0)
+try
 {
-    Write-Host "No Files found in the docker image container. Quiting"
-    exit 1
-}
-
-# If there are more than one files found, then a the environment had multiple msi files during docker build
-# and needs to be properly cleaned up first
-if($MSIFILES.Count -ne 1){
-    Write-Host "Found more than one msi file. Quiting"
-    foreach ($MSIFILE in $MSIFILES)
+    # If there are no files found, then there was a failure during the building of the docker image
+    if($MSIFILES.Count -eq 0)
     {
-        Write-Host "  $MSIFILE"
+        throw "No Files found in the docker image container."
     }
-    Write-Host "Quiting"
+
+    # If there are more than one files found, then a the environment had multiple msi files during docker build
+    # and needs to be properly cleaned up first
+    if($MSIFILES.Count -ne 1){
+
+        foreach ($MSIFILE in $MSIFILES)
+        {
+            Write-Host "Found  $MSIFILE"
+        }
+
+        throw "Found more than one msi file."
+    }
+
+}
+catch
+{
+    Write-Host "Unable to install msi file. Quiting"
     exit 1
 }
 
 # At this point there should only be a single msi file to run
-foreach ($MSIFILE in $MSIFILES) {
-    echo "Running msiexec on $MSIFILE"
-    & msiexec /i $MSIFILE /q /lp dockerinstall.log
-    if ($LASTEXITCODE -ne 0) {
-        Write-Host "Exit code is $LASTEXITCODE."
-        exit $Write-Host
+try{
+    foreach ($MSIFILE in $MSIFILES) {
+        echo "Running msiexec on $MSIFILE"
+        & msiexec /i $MSIFILE /q /lp dockerinstall.log
+        if ($LASTEXITCODE -ne 0) {
+            Write-Host "Exit code is $LASTEXITCODE."
+            throw "Problem running msiexec."
+        }
     }
 }
+catch
+{
+    Write-Host "Failed to install"
+    exit 1
+}
+
+
 
