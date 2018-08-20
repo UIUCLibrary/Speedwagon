f1948f93e787faaa7f23b00542f2794eaf61c094
Henry Borchers <henryborchers@yahoo.com>
2018-08-20 10:37:51 -0500

    comments

--- ci/docker/windowsserver/run_install.ps1
+++ ci/docker/windowsserver/run_install.ps1
@@ -1,20 +1,27 @@
 ﻿$MSIFILES = Get-ChildItem -Path . -Filter *.msi
 
+# Only continue if one and only one msi file is located
+
+# If there are no files found, then there was a failure during the building of the docker image
 if($MSIFILES.Count -eq 0)
 {
-    echo "No Files found"
+    Write-Host "No Files found in the docker image container. Quiting"
     exit 1
 }
 
+# If there are more than one files found, then a the environment had multiple msi files during docker build
+# and needs to be properly cleaned up first
 if($MSIFILES.Count -ne 1){
     Write-Host "Found more than one msi file. Quiting"
     foreach ($MSIFILE in $MSIFILES)
     {
-        echo "Found $MSIFILE"
+        Write-Host "  $MSIFILE"
     }
+    Write-Host "Quiting"
     exit 1
 }
 
+# At this point there should only be a single msi file to run
 foreach ($MSIFILE in $MSIFILES) {
     echo "$MSIFILE"
     msiexec /i $MSIFILE /q /lp dockerinstall.log
