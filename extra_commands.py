551320474711618b6d535f9cbcd73617e3d1691c
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-08-30 11:10:10 -0500

    extra build commands are located in extra_ccommands.py

---
+++ extra_commands.py
@@ -0,0 +1,111 @@
+import urllib.request
+from tempfile import TemporaryDirectory
+from distutils.command.clean import clean as _clean
+from distutils.command.build_py import build_py
+import setuptools.command
+import os
+import glob
+
+from pyqt_distutils.config import Config
+TESSERACT_DATA_URL = "https://github.com/tesseract-ocr/tessdata/raw/3.04.00/"
+
+
+class TesseractData(setuptools.Command):
+    description = "Download Tesseract data"
+    user_options = [
+        ("tessdata-dir=", "t", "Directory to save tesseract data "),
+        ("tessdata-url=", "u", "base url for downloading tesseract data "),
+        ('build-lib=', 'd', "directory to \"build\" (copy) to"),
+        # ('build-lib=', 'd', "directory to \"build\" (copy) to"),
+    ]
+
+    def initialize_options(self):
+        self.package_dir = None
+        self.build_lib = None
+        self.tessdata_url = None
+
+
+    def finalize_options(self):
+        self.set_undefined_options('build',
+                                   ('build_lib', 'build_lib'),
+                                   ('force', 'force'))
+
+        self.package_data = self.distribution.package_data
+        if self.tessdata_url is None:
+            self.tessdata_url = TESSERACT_DATA_URL
+
+    def run(self):
+        self.add_tesseract_data()
+
+    def add_tesseract_data(self):
+        destination = os.path.join(self.build_lib, "speedwagon", "workflows", "tessdata")
+
+        english_data_url = "{}{}".format(self.tessdata_url, "eng.traineddata")
+        self.download_data(english_data_url, destination)
+
+        osd_data_url = "{}{}".format(self.tessdata_url, "osd.traineddata")
+        self.download_data(osd_data_url, destination)
+
+
+    def download_data(self, url, destination):
+        with TemporaryDirectory() as download_path:
+            base_name = os.path.basename(url)
+            destination_file = os.path.join(destination, base_name)
+
+            if os.path.exists(destination_file):
+                return
+
+            # if not os.path.exists()
+            print("Downloading {}".format(url))
+            test_file_path = os.path.join(download_path, base_name)
+
+            urllib.request.urlretrieve(url, filename=test_file_path)
+            if not os.path.exists(test_file_path):
+                raise FileNotFoundError(
+                    "Failure to download file from {}".format(url))
+            self.move_file(test_file_path, destination)
+            # self.move_file(os.path.join(destination, "eng.traineddata"), "build/eng.traineddata")
+            # shutil.move(test_file_path, destination)
+
+
+class CustomBuildPy(build_py):
+    def run(self):
+        self.run_command("build_ui")
+        self.run_command("dl_tessdata")
+        super().run()
+
+
+class Clean(_clean):
+    def run(self):
+        super().run()
+        self.clean_ui()
+        self.clean_tesseract_data()
+
+    @staticmethod
+    def clean_tesseract_data():
+        base = os.path.abspath(os.path.dirname(__file__))
+        tessdata_path = os.path.join(
+            base, "speedwagon", "workflows", "tessdata"
+        )
+
+        glob_exp = os.path.join(tessdata_path, "*.traineddata")
+        for tesseract_data_file in glob.glob(glob_exp):
+            print("Removing {}".format(tesseract_data_file))
+            os.remove(tesseract_data_file)
+        #
+        # if os.path.exists(tessdata_path):
+        #     print("Removing {}".format(tessdata_path))
+        #     os.removedirs(tessdata_path)
+
+    @staticmethod
+    def clean_ui():
+        config = Config()
+        config.load()
+        for glob_exp, dest in config.files:
+            for src in glob.glob(glob_exp):
+
+                if src.endswith(".ui"):
+                    gen_file = "{}_ui.py".format(os.path.splitext(src)[0])
+                    if os.path.exists(gen_file):
+                        print("Removing {}".format(gen_file))
+                        os.remove(gen_file)
--- setup.py
+++ setup.py
@@ -3,63 +3,21 @@ import glob
 import os
 
 from setuptools import setup
-from distutils.command.clean import clean as _clean
-from distutils.command.build_py import build_py
+
+import extra_commands
 
 try:
     from pyqt_distutils.build_ui import build_ui
-    from pyqt_distutils.config import Config
-
-    class CustomBuildPy(build_py):
-        def run(self):
-            self.run_command("build_ui")
-            self.run_command("dl_tessdata")
-            super().run()
-
-    class Clean(_clean):
-        def run(self):
-            super().run()
-            self.clean_ui()
-            self.clean_tesseract_data()
-
-        @staticmethod
-        def clean_tesseract_data():
-            base = os.path.abspath(os.path.dirname(__file__))
-            tessdata_path = os.path.join(
-                base, "speedwagon", "workflows", "tessdata"
-            )
-
-            glob_exp = os.path.join(tessdata_path, "*.traineddata")
-            for tesseract_data_file in glob.glob(glob_exp):
-                print("Removing {}".format(tesseract_data_file))
-                os.remove(tesseract_data_file)
-
-            if os.path.exists(tessdata_path):
-                print("Removing {}".format(tessdata_path))
-                os.removedirs(tessdata_path)
-
-        @staticmethod
-        def clean_ui():
-            config = Config()
-            config.load()
-            for glob_exp, dest in config.files:
-                for src in glob.glob(glob_exp):
-
-                    if src.endswith(".ui"):
-                        gen_file = "{}_ui.py".format(os.path.splitext(src)[0])
-                        if os.path.exists(gen_file):
-                            print("Removing {}".format(gen_file))
-                            os.remove(gen_file)
-
-
     cmdclass = {
         "build_ui": build_ui,
-        "build_py": CustomBuildPy,
-        "clean": Clean,
+        "build_py": extra_commands.CustomBuildPy,
+        "clean": extra_commands.Clean,
     }
 
 except ImportError:
     cmdclass = {}
+
+cmdclass['dl_tessdata'] = extra_commands.TesseractData
 
 
 setup(
@@ -82,22 +40,27 @@ setup(
         "speedwagon",
         "speedwagon.tools",
         "speedwagon.workflows",
+        "speedwagon.workflows.tessdata",
         "speedwagon.ui"
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', "behave", "pytest-qt"],
     python_requires=">=3.6",
+
     entry_points={
         "gui_scripts": [
             'speedwagon = speedwagon.__main__:main'
         ],
-         "distutils.commands": [
-            "dl_tessdata = speedwagon.tessdata:TesseractData",
-        ],
     },
+    include_package_data=True,
     package_data={
+        'speedwagon.workflows.tessdata': [
+            'speedwagon/workflows/tessdata/*.*',
+            "speedwagon/workflows/tessdata/eng.traineddata",
+            "speedwagon/workflows/tessdata/osd.traineddata",
+        ],
         'speedwagon': ["favicon.ico"],
-        'speedwagon.workflows': ['speedwagon.workflows/tessdata/*.traineddata']
     },
     cmdclass=cmdclass,
 )
+
