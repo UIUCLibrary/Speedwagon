9f15be3b71a1a32b294e55bd7e900c443d9280ec
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-10-19 15:22:34 -0500

    use kdu_compress_cli2 instead of kdu_compress_cli

--- speedwagon/tools/tool_convertCaptureOnePreservationToDigitalLibJP2.py
+++ speedwagon/tools/tool_convertCaptureOnePreservationToDigitalLibJP2.py
@@ -65,9 +65,9 @@ class ConvertFile(AbsProcessStrategy):
                                         basename + ".jp2"
                                         )
 
-        rc = pykdu_compress.kdu_compress_cli(
-            "-i {} " "-o {}".format(source_file, output_file_path))
-
+        # rc = pykdu_compress.kdu_compress_cli(
+        #     "-i {} " "-o {}".format(source_file, output_file_path))
+        rc = pykdu_compress.kdu_compress_cli2(infile=source_file, outfile=output_file_path)
         if rc != 0:
             raise ProcessingException("kdu_compress_cli returned "
                                       "nonzero value: {}.".format(rc))
