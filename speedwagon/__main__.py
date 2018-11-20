08d04a1ab549e5ab7ff2d6a7547d069d73180a5c
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-11-20 13:18:47 -0600

    parse remaining CLI arguments after making sure that there is no --pytest argument

--- speedwagon/__main__.py
+++ speedwagon/__main__.py
@@ -10,11 +10,11 @@ logger.addHandler(logging.StreamHandler())
 
 
 def main():
-    startup_settings = speedwagon.startup.parse_args()
-
     if len(sys.argv) > 1 and sys.argv[1] == "--pytest":
         import pytest  # type: ignore
         sys.exit(pytest.main(sys.argv[2:]))
+
+    startup_settings = speedwagon.startup.parse_args()
 
     print("{}: {}".format(speedwagon.__name__, speedwagon.__version__))
     speedwagon.startup.main(startup_settings)
