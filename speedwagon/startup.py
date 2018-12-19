8688810ad8a57fd7a1c2d997f9b5695c859c2e55
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-12-19 13:12:28 -0600

    Speedwagon if configuring during a start up process

--- speedwagon/startup.py
+++ speedwagon/startup.py
@@ -6,7 +6,8 @@ import io
 import logging
 import os
 import sys
-from typing import Dict, Union
+from typing import Dict, Union, Iterator, Tuple
+import yaml
 
 import pkg_resources
 from PyQt5 import QtWidgets, QtGui, QtCore
@@ -111,132 +112,239 @@ def get_selection(all_workflows):
     return new_workflow_set
 
 
-def main() -> None:
-    platform_settings = speedwagon.config.get_platform_settings()
+def get_custom_tabs(all_workflows: dict, yaml_file)->Iterator[Tuple[str, dict]]:
+    try:
+        with open(yaml_file) as f:
+            tabs_config_data = yaml.load(f.read())
+        if tabs_config_data:
+            for tab_name in tabs_config_data:
 
-    config_file = os.path.join(
-        platform_settings.get_app_data_directory(), "config.ini")
+                try:
+                    new_tab_items = dict()
 
-    if not os.path.exists(config_file):
-        speedwagon.config.generate_default(config_file)
+                    for item_name in tabs_config_data.get(tab_name):
+                        try:
+                            new_tab_items[item_name] = all_workflows[item_name]
 
-    resolution_order = [
-        DefaultsSetter(),
-        ConfigFileSetter(config_file),
-        CliArgsSetter(),
-    ]
-    startup_settings = dict()
+                        except LookupError:
+                            print("Unable to load '{}' in tab {}.".format(
+                                item_name, tab_name), file=sys.stderr)
+                    yield tab_name, new_tab_items
+                except TypeError as e:
+                    print("Error loading tab '{}'. "
+                          "Reason: {}".format(tab_name, e), file=sys.stderr)
+                    continue
 
-    for settings_strategy in resolution_order:
-        startup_settings = settings_strategy.update(startup_settings)
+    except FileNotFoundError as e:
+        print("Custom tabs file not found. "
+              "Reason: {}".format(e), file=sys.stderr)
+    except AttributeError as e:
+        print("Custom tabs file failed to load. "
+              "Reason: {}".format(e), file=sys.stderr)
 
-    app = QtWidgets.QApplication(sys.argv)
-    logo = pkg_resources.resource_stream(__name__, "logo.png")
-    splash = QtWidgets.QSplashScreen(QtGui.QPixmap(logo.name).scaled(400, 400))
-
-    splash.setEnabled(False)
-    splash.setWindowFlags(
-        QtCore.Qt.WindowStaysOnTopHint |
-        QtCore.Qt.FramelessWindowHint
-    )
-    splash_message_handler = SplashScreenLogHandler(splash)
-    if startup_settings['debug']:
-        splash_message_handler.setLevel(logging.DEBUG)
-    else:
-        splash_message_handler.setLevel(logging.INFO)
-    splash.show()
-    app.processEvents()
-
-    icon = pkg_resources.resource_stream(__name__, "favicon.ico")
-    app.setWindowIcon(QtGui.QIcon(icon.name))
-    app.setApplicationVersion(f"{speedwagon.__version__}")
-    app.setApplicationDisplayName(f"{speedwagon.__name__.title()}")
-    app.processEvents()
-    with worker.ToolJobManager() as work_manager:
-        windows = MainWindow(work_manager=work_manager,
-                             debug=startup_settings['debug'])
-
-        windows.setWindowTitle("")
-        windows.log_manager.addHandler(splash_message_handler)
-        windows.log_manager.info(
-            f"{speedwagon.__name__.title()} {speedwagon.__version__}"
-        )
-        app.processEvents()
+    except yaml.YAMLError as e:
+        print("{} file failed to load. "
+              "Reason: {}".format(yaml_file, e), file=sys.stderr)
 
 
-        # ==================================================
-        # Load configurations
-        windows.log_manager.debug("Loading settings")
+class AbsStarter(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def run(self):
+        pass
+
+
+class StartupDefault(AbsStarter):
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+        self._logger.setLevel(logging.DEBUG)
+
+        self.handler = logging.StreamHandler(stream=sys.stderr)
+        self.handler.setLevel(logging.DEBUG)
+        self.platform_settings = speedwagon.config.get_platform_settings()
+
+        self.config_file = os.path.join(
+            self.platform_settings.get_app_data_directory(), "config.ini")
+
+        self.tabs_file = os.path.join(
+            self.platform_settings.get_app_data_directory(), "tabs.yml")
 
         # Make sure required directories exists
-        data_dir = platform_settings.get("user_data_directory")
-        if data_dir and not os.path.exists(data_dir):
-            os.makedirs(data_dir)
-            windows.log_manager.debug("Created directory {}".format(data_dir))
+        self.data_dir = self.platform_settings.get("user_data_directory")
+        self.startup_settings = dict()
+        self._debug = False
 
-        app_data_dir = platform_settings.get("app_data_directory")
+        self.app_data_dir = self.platform_settings.get("app_data_directory")
+        self.app = QtWidgets.QApplication(sys.argv)
 
-        if app_data_dir is not None and not os.path.exists(app_data_dir):
-            os.makedirs(app_data_dir)
-            windows.log_manager.debug("Created {}".format(app_data_dir))
+    def run(self):
+        self.ensure_settings_files()
+        self.resolve_settings()
 
-        with speedwagon.config.ConfigManager(config_file) as f:
-            platform_settings._data.update(f.global_settings)
-        # windows.user_settings = platform_settings
-        work_manager.user_settings = platform_settings
-        work_manager.configuration_file = config_file
-        # ==================================================
-        if startup_settings['debug'] is True:
+        # Display a splash screen until the app is loaded
+        logo = pkg_resources.resource_stream(__name__, "logo.png")
+
+        splash = QtWidgets.QSplashScreen(
+            QtGui.QPixmap(logo.name).scaled(400, 400))
+
+        splash.setEnabled(False)
+        splash.setWindowFlags(
+            QtCore.Qt.WindowStaysOnTopHint |
+            QtCore.Qt.FramelessWindowHint
+        )
+        splash_message_handler = SplashScreenLogHandler(splash)
+
+        # If debug mode, print the log messages directly on the splash screen
+        if self._debug:
             splash_message_handler.setLevel(logging.DEBUG)
-            windows.log_manager.debug("DEBUG mode")
-
         else:
             splash_message_handler.setLevel(logging.INFO)
 
-        # ==================================================
-        windows.log_manager.debug("Loading Tools")
+        splash.show()
+        self.app.processEvents()
 
-        loading_job_stream = io.StringIO()
+        self.set_app_display_metadata()
 
-        with contextlib.redirect_stderr(loading_job_stream):
-            tools = job.available_tools()
-            windows.add_tools(tools)
+        with worker.ToolJobManager() as work_manager:
 
-        tool_error_msgs = loading_job_stream.getvalue().strip()
-        if tool_error_msgs:
-            for line in tool_error_msgs.split("\n"):
-                windows.log_manager.warn(line)
+            work_manager.settings_path = \
+                self.platform_settings.get_app_data_directory()
 
-        # ==================================================
-        windows.log_manager.debug("Loading Workflows")
+            windows = MainWindow(work_manager=work_manager,
+                                 debug=self.startup_settings['debug'])
 
-        loading_workflows_stream = io.StringIO()
+            windows.setWindowTitle("")
+            self._logger.addHandler(splash_message_handler)
 
-        with contextlib.redirect_stderr(loading_workflows_stream):
-            all_workflows = job.available_workflows()
-            selection_workflows = get_selection(all_workflows)
-            windows.add_tab("Subsection", selection_workflows)
-            windows.add_tab("All", collections.OrderedDict(sorted(all_workflows.items())))
-        workflow_errors_msg = loading_workflows_stream.getvalue().strip()
+            self._logger.addHandler(windows.log_data_handler)
+            self._logger.addHandler(windows.console_log_handler)
 
-        if workflow_errors_msg:
-            for line in workflow_errors_msg.split("\n"):
-                windows.log_manager.warn(line)
+            app_title = speedwagon.__name__.title()
+            app_version = speedwagon.__version__
 
-        # ==================================================
-        windows.log_manager.debug("Loading User Interface")
-        windows.show()
+            self._logger.info(f"{app_title} {app_version}"
+            )
+            self.app.processEvents()
 
-        windows.log_manager.removeHandler(splash_message_handler)
-        if "starting-tab" in startup_settings:
-            windows.set_current_tab(tab_name=startup_settings['starting-tab'])
+            # ==================================================
+            # Load configurations
+            self._logger.debug("Applying settings to Speedwagon")
 
-        # QtCore.QThread.sleep(1)
-        splash.finish(windows)
+            work_manager.user_settings = self.platform_settings
+            work_manager.configuration_file = self.config_file
 
-        windows.log_manager.info("Ready")
-        rc = app.exec_()
-    sys.exit(rc)
+            # ==================================================
+            self._logger.debug("Loading Tools")
+
+            loading_job_stream = io.StringIO()
+
+            with contextlib.redirect_stderr(loading_job_stream):
+                tools = job.available_tools()
+                windows.add_tools(tools)
+
+            tool_error_msgs = loading_job_stream.getvalue().strip()
+            if tool_error_msgs:
+                for line in tool_error_msgs.split("\n"):
+                    self._logger.warning(line)
+
+            # ==================================================
+            self._logger.debug("Loading Workflows")
+            loading_workflows_stream = io.StringIO()
+            with contextlib.redirect_stderr(loading_workflows_stream):
+                all_workflows = job.available_workflows()
+
+            # Load every user configured tab
+            for tab_name, extra_tab in \
+                    get_custom_tabs(all_workflows, self.tabs_file):
+
+                windows.add_tab(tab_name, collections.OrderedDict(
+                    sorted(extra_tab.items())))
+
+            # All Workflows tab
+
+            self._logger.debug("Loading Tab All")
+            windows.add_tab("All", collections.OrderedDict(
+                sorted(all_workflows.items())))
+
+            workflow_errors_msg = loading_workflows_stream.getvalue().strip()
+
+            if workflow_errors_msg:
+                for line in workflow_errors_msg.split("\n"):
+                    self._logger.warning(line)
+
+            # ==================================================
+            self._logger.debug("Loading User Interface")
+
+            windows.show()
+
+            if "starting-tab" in self.startup_settings:
+                windows.set_current_tab(
+                    tab_name=self.startup_settings['starting-tab'])
+
+            # QtCore.QThread.sleep(1)
+            splash.finish(windows)
+
+            self._logger.info("Ready")
+            self._logger.removeHandler(windows.log_data_handler)
+            self._logger.removeHandler(windows.console_log_handler)
+            self._logger.removeHandler(splash_message_handler)
+            return self.app.exec_()
+
+    def read_settings_file(self, settings_file):
+        with speedwagon.config.ConfigManager(settings_file) as f:
+            self.platform_settings._data.update(f.global_settings)
+
+    def set_app_display_metadata(self):
+        icon = pkg_resources.resource_stream(__name__, "favicon.ico")
+        self.app.setWindowIcon(QtGui.QIcon(icon.name))
+        self.app.setApplicationVersion(f"{speedwagon.__version__}")
+        self.app.setApplicationDisplayName(f"{speedwagon.__name__.title()}")
+        self.app.processEvents()
+
+    def resolve_settings(self):
+        resolution_order = [
+            DefaultsSetter(),
+            ConfigFileSetter(self.config_file),
+            CliArgsSetter(),
+        ]
+        # self.read_settings_file(self.config_file)
+        for settings_strategy in resolution_order:
+            self.startup_settings = settings_strategy.update(
+                self.startup_settings)
+        try:
+            self._debug = self.startup_settings['debug']
+        except KeyError:
+            self._logger.warning(
+                "Unable to find a key for debug mode, setting false")
+
+            self._debug = False
+
+    def ensure_settings_files(self):
+        if not os.path.exists(self.config_file):
+            speedwagon.config.generate_default(self.config_file)
+
+            self._logger.debug(
+                "No config file found. Generated {}".format(self.config_file))
+
+        if not os.path.exists(self.tabs_file):
+            with open(self.tabs_file, "w"):
+                pass
+            self._logger.debug(
+                "No tabs.yml file found. Generated {}".format(self.tabs_file))
+
+        if self.data_dir and not os.path.exists(self.data_dir):
+            os.makedirs(self.data_dir)
+            self._logger.debug("Created directory {}".format(self.data_dir))
+
+        if self.app_data_dir is not None and \
+                not os.path.exists(self.app_data_dir):
+
+            os.makedirs(self.app_data_dir)
+            self._logger.debug("Created {}".format(self.app_data_dir))
+            # windows.log_manager.debug("Created {}".format(app_data_dir))
+
+def main() -> None:
+    app = StartupDefault()
+    sys.exit(app.run())
 
 
 if __name__ == '__main__':
