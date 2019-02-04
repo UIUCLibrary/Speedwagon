69e39f6673f13c0db4f0ec0a143f4f7297f4109d
Borchers, Henry Samuel <hborcher@illinois.edu>
2019-02-04 15:30:43 -0600

    PSR-156 Convert to Workflow API: Convert CaptureOne TIFF to Hathi TIFF package

--- speedwagon/workflows/workflow_capture_one_to_hathi.py
+++ speedwagon/workflows/workflow_capture_one_to_hathi.py
@@ -1,137 +1,13 @@
 import logging
-from typing import Type, List, Any
+from typing import List, Any
 from contextlib import contextmanager
-
-import os
-
+from uiucprescon import packager
+from speedwagon import tasks
 from speedwagon.job import AbsWorkflow
-from speedwagon import worker
-from speedwagon.job import AbsTool
 from speedwagon.tools import options
-from speedwagon.worker import ProcessJobWorker, GuiLogHandler
+from speedwagon.worker import GuiLogHandler
 
-import uiucprescon.packager
-import uiucprescon.packager.packages
 from uiucprescon.packager.packages.collection_builder import Metadata
-import enum
-
-
-class UserArgs(enum.Enum):
-    INPUT = "Input"
-    OUTPUT = "Output"
-
-
-class ResultValues(enum.Enum):
-    VALID = "valid"
-    FILENAME = "filename"
-    PATH = "path"
-
-
-class JobValues(enum.Enum):
-    PACKAGE = "package"
-    OUTPUT = "output"
-    SOURCE_PATH = "source_path"
-
-
-class CaptureOneToHathiTiffPackage(AbsTool):
-    name = "Convert CaptureOne TIFF to Hathi TIFF package"
-    description = "Input is a path to a folder of TIFF files all named with " \
-                  "a bibID as a prefacing identifier, a final delimiting " \
-                  "underscore or dash, and a sequence consisting of padded " \
-                  "zeroes and a number." \
-                  "\n" \
-                  "\nOutput is a directory of folders named by bibID with " \
-                  "the prefacing delimiter stripped from each filename."\
-                  "\n" \
-                  "\nInput:" \
-                  "\n  + batch folder" \
-                  "\n      - Bibid1_00000001.tif" \
-                  "\n      - Bibid1_00000002.tif" \
-                  "\n      - Bibid1_00000003.tif" \
-                  "\n      - Bibid2_00000001.tif" \
-                  "\n      - Bibid2_00000002.tif" \
-                  "\n" \
-                  "\nOutput:" \
-                  "\n  + Bibid1 (folder)" \
-                  "\n      - 00000001.tif" \
-                  "\n      - 00000002.tif" \
-                  "\n      - 00000003.tif" \
-                  "\n  + Bibid2 (folder)" \
-                  "\n      - 00000001.tif" \
-                  "\n      - 00000002.tif"
-
-    @staticmethod
-    def discover_task_metadata(**user_args) -> List[dict]:
-        jobs = []
-        source_input = user_args[UserArgs.INPUT.value]
-        dest = user_args[UserArgs.OUTPUT.value]
-
-        package_factory = uiucprescon.packager.PackageFactory(
-            uiucprescon.packager.packages.CaptureOnePackage())
-
-        for package in package_factory.locate_packages(source_input):
-            jobs.append({
-                JobValues.PACKAGE.value: package,
-                JobValues.OUTPUT.value: dest,
-                JobValues.SOURCE_PATH.value: source_input
-            }
-            )
-        return jobs
-
-    @staticmethod
-    def new_job() -> Type[worker.ProcessJobWorker]:
-        return PackageConverter
-
-    @staticmethod
-    def get_user_options() -> List[options.UserOption2]:
-        return [
-            options.UserOptionCustomDataType(UserArgs.INPUT.value,
-                                             options.FolderData),
-
-            options.UserOptionCustomDataType(UserArgs.OUTPUT.value,
-                                             options.FolderData),
-        ]
-
-    @staticmethod
-    def validate_user_options(**user_args):
-        if not os.path.exists(user_args[UserArgs.INPUT.value]) \
-                or not os.path.isdir(user_args[UserArgs.INPUT.value]):
-
-            raise ValueError("Invalid value in input ")
-
-        if not os.path.exists(user_args[UserArgs.OUTPUT.value]) \
-                or not os.path.isdir(user_args[UserArgs.OUTPUT.value]):
-
-            raise ValueError("Invalid value in output ")
-
-
-class PackageConverter(ProcessJobWorker):
-
-    @contextmanager
-    def log_config(self, logger):
-        gui_logger = GuiLogHandler(self.log)
-        try:
-            logger.addHandler(gui_logger)
-            yield
-        finally:
-            logger.removeHandler(gui_logger)
-
-    def process(self, *args, **kwargs):
-        my_logger = logging.getLogger(uiucprescon.packager.__name__)
-        my_logger.setLevel(logging.INFO)
-        with self.log_config(my_logger):
-            existing_package = kwargs[JobValues.PACKAGE.value]
-            new_package_root = kwargs[JobValues.OUTPUT.value]
-            source_path = kwargs[JobValues.SOURCE_PATH.value]
-            package_id = existing_package.metadata[Metadata.ID]
-
-            self.log(f"Converting {package_id} from {source_path} to a "
-                     f"Hathi Trust Tiff package at {new_package_root}")
-
-            package_factory = uiucprescon.packager.PackageFactory(
-                uiucprescon.packager.packages.HathiTiff())
-
-            package_factory.transform(existing_package, dest=new_package_root)
 
 
 class CaptureOneToHathiTiffPackageWorkflow(AbsWorkflow):
@@ -145,14 +21,79 @@ class CaptureOneToHathiTiffPackageWorkflow(AbsWorkflow):
                   "It takes as its input a folder of CaptureOne batch " \
                   "files. It takes as its output a folder location where " \
                   "new files will be written."
-    active = False
+    active = True
 
     def discover_task_metadata(self, initial_results: List[Any],
                                additional_data, **user_args) -> List[dict]:
 
-        jobs = CaptureOneToHathiTiffPackage.discover_task_metadata(**user_args)
+        jobs = []
+        source_input = user_args["Input"]
+        dest = user_args["Output"]
+
+        package_factory = packager.PackageFactory(
+            packager.packages.CaptureOnePackage())
+
+        for package in package_factory.locate_packages(source_input):
+            jobs.append({
+                "package": package,
+                "output": dest,
+                "source_path": source_input
+            }
+            )
         return jobs
+        # return jobs
 
     def user_options(self):
-        options = CaptureOneToHathiTiffPackage.get_user_options()
-        return options
+        return [
+            options.UserOptionCustomDataType("Input", options.FolderData),
+            options.UserOptionCustomDataType("Output", options.FolderData)
+            ]
+
+    def create_new_task(self, task_builder: tasks.TaskBuilder, **job_args):
+        existing_package = job_args['package']
+        new_package_root = job_args["output"]
+        source_path = job_args["source_path"]
+        package_id = existing_package.metadata[Metadata.ID]
+
+        packaging_task = PackageConverter(
+            source_path=source_path,
+            existing_package=existing_package,
+            new_package_root=new_package_root,
+            packaging_id=package_id
+
+        )
+        task_builder.add_subtask(packaging_task)
+
+
+class PackageConverter(tasks.Subtask):
+    @contextmanager
+    def log_config(self, logger):
+        gui_logger = GuiLogHandler(self.log)
+        try:
+            logger.addHandler(gui_logger)
+            yield
+        finally:
+            logger.removeHandler(gui_logger)
+
+    def __init__(self, source_path, packaging_id,
+                 existing_package, new_package_root) -> None:
+
+        super().__init__()
+        self.packaging_id = packaging_id
+        self.existing_package = existing_package
+        self.new_package_root = new_package_root
+        self.source_path = source_path
+
+    def work(self):
+        my_logger = logging.getLogger(packager.__name__)
+        my_logger.setLevel(logging.INFO)
+        with self.log_config(my_logger):
+            self.log(f"Converting {self.packaging_id} from "
+                     f"{self.source_path} to a Hathi Trust Tiff "
+                     f"package at {self.new_package_root}")
+
+            package_factory = packager.PackageFactory(
+                packager.packages.HathiTiff())
+            package_factory.transform(self.existing_package,
+                                      dest=self.new_package_root)
+        return True
