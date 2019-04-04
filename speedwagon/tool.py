ba9c94595ec07f636aa3d002f708d9860d1d90ad
Borchers, Henry Samuel <hborcher@illinois.edu>
2019-04-04 16:36:51 -0500

    added

---
+++ speedwagon/tool.py
@@ -0,0 +1,60 @@
+import abc
+import os
+from typing import Type, List
+
+from speedwagon import worker
+from speedwagon import job
+from speedwagon.job import AbsDynamicFinder
+from speedwagon.tools.options import UserOption2
+
+
+class AbsTool(job.AbsJob):
+
+    @staticmethod
+    @abc.abstractmethod
+    def new_job() -> Type[worker.ProcessJobWorker]:
+        pass
+
+    @staticmethod
+    def discover_jobs(**user_args) -> List[dict]:
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def get_user_options() -> List[UserOption2]:
+        pass
+
+    @staticmethod
+    def post_process(user_args: dict):
+        pass
+
+    @staticmethod
+    def on_completion(*args, **kwargs):
+        pass
+
+    def user_options(self):
+        return self.get_user_options()
+
+    def discover_task_metadata(self, **user_args) -> List[dict]:
+        return self.discover_jobs(**user_args)
+
+    @staticmethod
+    def generate_report(results, user_args):
+        return None
+
+
+class ToolFinder(AbsDynamicFinder):
+
+    @staticmethod
+    def py_module_filter(item: os.DirEntry):
+        if not str(item.name).startswith("tool_"):
+            return False
+        return True
+
+    @property
+    def package_name(self) -> str:
+        return "{}.tools".format(__package__)
+
+    @property
+    def base_class(self):
+        return AbsTool
