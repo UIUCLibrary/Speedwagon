3ae08c3cbd30f2fb0953dbc41e5a572064fe4cc0
henry <henryborchers@yahoo.com>
2020-07-01 15:10:48 -0500

    PSR-182 Tools for HathiTrust Limited View Legacy Content to Digital Library: run a test that completes the conversion

--- speedwagon/workflows/workflow_hathi_limited_to_dl_compound.py
+++ speedwagon/workflows/workflow_hathi_limited_to_dl_compound.py
@@ -1,22 +1,57 @@
+import sys
 from typing import List, Any
 
 from speedwagon.job import Workflow
 from . import shared_custom_widgets as options
+from uiucprescon import packager
+
+from .. import tasks
 
 
 class HathiLimitedToDLWorkflow(Workflow):
     name = "Convert HathiTrust limited view to Digital library"
-    description = 'This tool converts HathiTrust limited view packages to '\
+    description = 'This tool converts HathiTrust limited view packages to ' \
                   'Digital library'
 
     active = True
 
     def discover_task_metadata(self, initial_results: List[Any],
                                additional_data, **user_args) -> List[dict]:
-        pass
+        hathi_limited_view_packager = packager.PackageFactory(
+            packager.packages.HathiLimitedView())
+        tasks = []
+
+        for p in hathi_limited_view_packager.locate_packages(
+                user_args['Input']):
+            tasks.append({
+                "package": p,
+                "destination": user_args['Output']
+            })
+
+        return tasks
+
+    def create_new_task(self, task_builder: tasks.TaskBuilder, **job_args):
+        n = PackageConverter(src=job_args['package'],
+                             dst=job_args['destination'])
+        task_builder.add_subtask(n)
 
     def user_options(self):
         return [
             options.UserOptionCustomDataType("Input", options.FolderData),
             options.UserOptionCustomDataType("Output", options.FolderData)
         ]
+
+
+class PackageConverter(tasks.Subtask):
+
+    def work(self) -> bool:
+        output_packager = packager.PackageFactory(
+            packager.packages.DigitalLibraryCompound())
+        output_packager.transform(self.src, self.dst)
+        return True
+
+    def __init__(self, src, dst) -> None:
+
+        super().__init__()
+        self.src = src
+        self.dst = dst
--- tests/workflows/test_hathi_limited_to_dl_compound.py
+++ tests/workflows/test_hathi_limited_to_dl_compound.py
@@ -1,23 +1,114 @@
 import logging
+import os
+import shutil
+from zipfile import ZipFile
+
+import pykdu_compress
 import pytest
+from uiucprescon import packager
+
 from speedwagon.workflows.workflow_hathi_limited_to_dl_compound import \
     HathiLimitedToDLWorkflow
 
 
-def test_hathi_limited_to_dl_compound_run(tool_job_manager_spy, tmpdir):
-    sample_pkg_dir = tmpdir / "sample"
-    sample_pkg_dir.mkdir()
-    sample_file = sample_pkg_dir / "dummy.txt"
-    sample_file.write_text("", encoding="utf8")
-    my_logger = logging.getLogger()
+
+@pytest.fixture(scope="module")
+def hathi_limited_view_package_dirs(tmpdir_factory):
+    test_dir = tmpdir_factory.mktemp(f"hathi_limited", numbered=True)
+    sample_package_names = {
+        "uiuc.40": [
+            (
+                "40.mets.xml",
+                (
+                    "40",
+                    [
+                        "40.mets.xml"
+                    ] +
+                    [f"{str(a).zfill(7)}.txt" for a in range(282)] +
+                    [f"{str(a).zfill(7)}.jp2" for a in range(282)] +
+                    [f"{str(a).zfill(7)}.xml" for a in range(282)]
+                )
+            )
+        ],
+        "uiuc.40834v1": [
+            (
+                "40834v1.mets.xml",
+                (
+                    "40834v1",
+                    [
+                        "40834v1.mets.xml"
+                    ] +
+                    [f"{str(a).zfill(7)}.txt" for a in range(256)] +
+                    [f"{str(a).zfill(7)}.tif" for a in range(256)] +
+                    [f"{str(a).zfill(7)}.xml" for a in range(256)]
+                )
+            )
+        ],
+        "uiuc.5285248v1924": [
+            (
+                "5285248v1924.mets.xml",
+                (
+                    "5285248v1924",
+                    [
+                        "5285248v1924.mets.xml"
+                    ] +
+                    [f"{str(a).zfill(7)}.txt" for a in range(282)] +
+                    [f"{str(a).zfill(7)}.jp2" for a in range(282)] +
+                    [f"{str(a).zfill(7)}.xml" for a in range(282)]
+                )
+            )
+        ]
+    }
+
+
+    # pkg_data = sample_package_names[request.param]
+    # eg: 5285248v1924/
+    for pkg_name, pkg_data in sample_package_names.items():
+        pkg_dir = test_dir.mkdir(pkg_name)
+
+        tmp_dir = test_dir.mkdir(f"build_dir-{pkg_name}")
+        for mets_file_filename, archive_data in pkg_data:
+            # Add any files to the package
+            pkg_dir.join(mets_file_filename).write("")
+            bib_id, zip_content = archive_data
+
+            # eg: 5285248v1924/5285248v1924.zip
+            with ZipFile(pkg_dir.join(f"{bib_id}.zip"), 'w') as myzip:
+                build_package_dir = tmp_dir.mkdir(bib_id)
+                for zipped_file in zip_content:
+                    generated_file = build_package_dir.join(zipped_file)
+                    generated_file.write("")
+
+                    arcname = os.path.join(bib_id, zipped_file)
+                    myzip.write(generated_file, arcname=arcname)
+
+    return test_dir
+
+
+def test_hathi_limited_to_dl_compound_run(tool_job_manager_spy,
+                                          hathi_limited_view_package_dirs,
+                                          monkeypatch,
+                                          tmpdir):
+    output_dir = tmpdir / "output"
+    output_dir.mkdir()
+    my_logger = logging.getLogger(__file__)
+
+    def mock_kdu_convert(infile: str, outfile: str, in_args=None, out_args=None):
+        shutil.copyfile(infile, outfile)
+
+    monkeypatch.setattr(pykdu_compress, "kdu_compress_cli2", mock_kdu_convert)
+    monkeypatch.setattr(pykdu_compress, "kdu_expand_cli", mock_kdu_convert)
+
     tool_job_manager_spy.run(None,
                HathiLimitedToDLWorkflow(),
                options={
-                   "Input": sample_pkg_dir.realpath()},
+                   "Input": str(hathi_limited_view_package_dirs),
+                   "Output": str(output_dir.realpath())
+               },
                logger=my_logger)
 
-    # exp_res = os.path.join(sample_pkg_dir.realpath(), "checksum.md5")
-    # assert os.path.exists(exp_res)
+    exp_res = output_dir / "40"
+    assert os.path.exists(exp_res.realpath()), f"Missing expected directory '{exp_res.relto(tmpdir)}'"
 
 options = [
     (0, "Input"),
