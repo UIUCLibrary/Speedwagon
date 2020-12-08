b0a3230c56c5942609c80ef14354ee3f942904c2
henry <henryborchers@yahoo.com>
2020-12-08 12:12:25 -0600

    package is now pkg

--- ci/jenkins/scripts/sonarqube.groovy
+++ ci/jenkins/scripts/sonarqube.groovy
@@ -32,19 +32,19 @@ def get_sonarqube_unresolved_issues(report_task_file){
 
 def submitToSonarcloud(args = [:]){
     echo "args = ${args}"
-    def package = args['package']
+    def pkg = args.package
     def isPullRequest = args['pullRequest'] ? true: false
     def buildString = args['buildString'] ? args['buildString']: env.BUILD_TAG
     withSonarQubeEnv(installationName:"sonarcloud", credentialsId: 'sonarcloud-speedwagon') {
         if (isPullRequest == true){
             runSonarScanner(
-                projectVersion: package.version,
+                projectVersion: pkg.version,
                 buildString: buildString,
                 pullRequest: args['pullRequest']
             )
         } else{
             runSonarScanner(
-                projectVersion: package.version,
+                projectVersion: pkg.version,
                 buildString: buildString,
             )
         }
