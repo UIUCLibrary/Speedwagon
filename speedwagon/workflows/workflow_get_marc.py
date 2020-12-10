03c537fe358c724661e0286eb43c17864609bc94
henry <henryborchers@yahoo.com>
2020-12-10 15:53:05 -0600

    get marc uses new server

--- speedwagon/workflows/workflow_get_marc.py
+++ speedwagon/workflows/workflow_get_marc.py
@@ -1,6 +1,9 @@
+import abc
 import os
 import re
 from typing import List, Any, Optional
+
+import requests
 
 from speedwagon import tasks, reports
 from speedwagon.job import AbsWorkflow
@@ -21,30 +24,38 @@ class GenerateMarcXMLFilesWorkflow(AbsWorkflow):
                   "retrieve these MARC.XML files from the Library’s catalog."
 
     def user_options(self):
-        return [
-            options.UserOptionCustomDataType("Input",
-                                             options.FolderData),
+
+        workflow_options = [
+            options.UserOptionCustomDataType("Input", options.FolderData)
         ]
+        id_type_option = options.ListSelection("Identifier type")
+        for id_type in SUPPORTED_IDENTIFIERS.keys():
+            id_type_option.add_selection(id_type)
+        return workflow_options
+
+    @classmethod
+    def filter_bib_id_folders(cls, item: os.DirEntry):
+
+        if not item.is_dir():
+            return False
+
+        if "v" not in item.name and not isinstance(eval(item.name), int):
+            return False
+
+        return True
 
     def discover_task_metadata(self, initial_results: List[Any],
                                additional_data, **user_args) -> List[dict]:
         jobs = []
 
-        def filter_bib_id_folders(item: os.DirEntry):
-
-            if not item.is_dir():
-                return False
-
-            if "v" not in item.name and not isinstance(eval(item.name), int):
-                return False
-
-            return True
-
-        for folder in filter(filter_bib_id_folders,
+        for folder in filter(self.filter_bib_id_folders,
                              os.scandir(user_args["Input"])):
 
             jobs.append({
-                "bib_id": folder.name,
+                "identifier":{
+                    "value": folder.name,
+                    "type": user_args['Identifier type'],
+                },
                 "path": folder.path
             })
         return jobs
@@ -57,9 +68,15 @@ class GenerateMarcXMLFilesWorkflow(AbsWorkflow):
             raise ValueError("Invalid value in input ")
 
     def create_new_task(self, task_builder: tasks.TaskBuilder, **job_args):
-        bib_id = job_args["bib_id"]
+        identifier = job_args['identifier']["value"]
+        identifier_type = job_args['identifier']["type"]
+
         folder = job_args["path"]
-        new_task = MarcGeneratorTask(bib_id, folder)
+        new_task = MarcGenerator2Task(
+            identifier=identifier,
+            identifier_type=identifier_type,
+            output_name=os.path.join(folder, "MARC.XML")
+        )
 
         task_builder.add_subtask(new_task)
 
@@ -93,6 +110,75 @@ class GenerateMarcXMLFilesWorkflow(AbsWorkflow):
         return message
 
 
+class AbsMarcFileStrategy(abc.ABC):
+    @abc.abstractmethod
+    def get_record(self, ident) -> str:
+        pass
+
+
+class GetMarcBibId(AbsMarcFileStrategy):
+    def get_record(self, ident) -> str:
+        # todo: get a marc record from the new server using bibid
+        pass
+
+
+class GetMarcMMSID(AbsMarcFileStrategy):
+    def get_record(self, ident) -> str:
+        r = requests.get(f"https://getmarc.library.illinois.edu/api/record?mms_id={ident}")
+        r.raise_for_status()
+        return r.text
+
+
+class OldStyleMarc(AbsMarcFileStrategy):
+
+    def get_record(self, ident) -> str:
+        marc = pygetmarc.get_marc(int(ident))
+
+        field_adder = pygetmarc.modifiers.Add955()
+        field_adder.bib_id = ident
+        if "v" in ident:
+            field_adder.contains_v = True
+
+        enriched_marc = field_adder.enrich(src=marc)
+
+        reflow_modifier = pygetmarc.modifiers.Reflow()
+        return reflow_modifier.enrich(enriched_marc)
+
+
+def strip_volume(full_bib_id: str) -> int:
+    # Only pull the base bib id
+    volume_regex = re.compile("^[0-9]{7}(?=((v[0-9]*)((i[0-9])?)?)?$)")
+    result = volume_regex.match(full_bib_id)
+    if not result:
+        raise ValueError("{} is not a valid bib_id".format(full_bib_id))
+    return int(result.group(0))
+
+
+SUPPORTED_IDENTIFIERS = {
+    "MMS ID": GetMarcMMSID(),
+    "Bibid": GetMarcBibId()
+}
+
+
+class MarcGenerator2Task(tasks.Subtask):
+
+    def __init__(self, identifier, identifier_type, output_name):
+        super().__init__()
+        self._identifier = identifier
+        self._identifier_type = identifier_type
+        self._output_name = output_name
+
+
+    def work(self) -> bool:
+        strat = SUPPORTED_IDENTIFIERS[self._identifier_type]
+        r = strat.get_record(self._identifier)
+
+        with open(self._output_name, "w") as wf:
+            wf.write(r)
+        self.set_results({"Input":"d", "bib_id": "d", "output": self._output_name})
+        return True
+
+
 class MarcGeneratorTask(tasks.Subtask):
 
     def __init__(self, bib_id, folder) -> None:
@@ -107,19 +193,21 @@ class MarcGeneratorTask(tasks.Subtask):
 
         self.log(f"Retrieving {out_file_name} for {self._bib_id}")
         try:
-            short_bibid = strip_volume(self._bib_id)
-            marc = pygetmarc.get_marc(int(short_bibid))
+            marc_retriever = OldStyleMarc()
 
-            field_adder = pygetmarc.modifiers.Add955()
-            field_adder.bib_id = self._bib_id
-            if "v" in self._bib_id:
-                field_adder.contains_v = True
-
-            enriched_marc = field_adder.enrich(src=marc)
-
-            reflow_modifier = pygetmarc.modifiers.Reflow()
-            cleaned_up_marc = reflow_modifier.enrich(enriched_marc)
-
+            # short_bibid = strip_volume(self._bib_id)
+            # marc = pygetmarc.get_marc(int(short_bibid))
+            #
+            # field_adder = pygetmarc.modifiers.Add955()
+            # field_adder.bib_id = self._bib_id
+            # if "v" in self._bib_id:
+            #     field_adder.contains_v = True
+            #
+            # enriched_marc = field_adder.enrich(src=marc)
+            #
+            # reflow_modifier = pygetmarc.modifiers.Reflow()
+            # cleaned_up_marc = reflow_modifier.enrich(enriched_marc)
+            cleaned_up_marc = marc_retriever.get_record(self._bib_id)
             with open(dst, "w", encoding="utf-8-sig") as f:
                 f.write(f"{cleaned_up_marc}\n")
             self.log(f"Generated {dst}")
@@ -137,12 +225,3 @@ class MarcGeneratorTask(tasks.Subtask):
         self.set_results(result)
 
         return True
-
-
-def strip_volume(full_bib_id: str) -> int:
-    # Only pull the base bib id
-    volume_regex = re.compile("^[0-9]{7}(?=((v[0-9]*)((i[0-9])?)?)?$)")
-    result = volume_regex.match(full_bib_id)
-    if not result:
-        raise ValueError("{} is not a valid bib_id".format(full_bib_id))
-    return int(result.group(0))
---
+++ tests/workflows/test_get_marc.py
@@ -0,0 +1,77 @@
+import os
+from unittest.mock import MagicMock, Mock
+
+import requests
+
+from speedwagon.workflows import workflow_get_marc
+import pytest
+import logging
+
+from speedwagon.workflows.workflow_get_marc import MarcGenerator2Task
+
+
+@pytest.fixture
+def unconfigured_workflow():
+    workflow = workflow_get_marc.GenerateMarcXMLFilesWorkflow()
+    user_options = {i.label_text: i.data for i in workflow.user_options()}
+    return workflow, user_options
+
+
+def test_input_dir_is_valid(tmp_path, unconfigured_workflow):
+    workflow, user_options = unconfigured_workflow
+    test_pkg = tmp_path / "4717"
+    test_pkg.mkdir()
+
+    user_options["Input"] = str(test_pkg.resolve())
+    user_options["Identifier type"] = "MMS ID"
+    workflow.validate_user_options(**user_options)
+
+
+def test_invalid_input_dir_raises(unconfigured_workflow):
+    workflow, user_options = unconfigured_workflow
+    with pytest.raises(ValueError):
+        workflow.validate_user_options(Input="Invalid path")
+
+
+def test_discover_metadata(unconfigured_workflow, monkeypatch):
+
+    workflow, user_options = unconfigured_workflow
+    user_options["Identifier type"] = "MMS ID"
+    workflow.filter_bib_id_folders = Mock(return_value=True)
+
+    def get_data(*args, **kwargs):
+        # name attribute can't be mocked in the constructor for Mock
+        first = Mock(path="/fakepath/99101026212205899")
+        first.name = "99101026212205899"
+
+        second = Mock(path="/fakepath/99954806053105899")
+        second.name = "99954806053105899"
+
+        return [first, second]
+
+    monkeypatch.setattr(os, 'scandir', get_data)
+
+    user_options["Input"] = "/fakepath"
+    jobs = workflow.discover_task_metadata([], None, **user_options)
+    assert len(jobs) == 2
+    assert jobs[0]['path'] == "/fakepath/99101026212205899"
+    assert jobs[0]['identifier']['value'] == "99101026212205899"
+
+
+def test_task_creates_file(tmp_path, monkeypatch):
+    expected_file = tmp_path / "MARC.XML"
+
+    task = MarcGenerator2Task(
+        identifier="99101026212205899",
+        identifier_type="MMS ID",
+        output_name=expected_file
+    )
+    def mock_get(*args, **kwargs):
+        result = Mock(text="/fakepath/99101026212205899")
+        result.raise_for_status = MagicMock(return_value=None)
+        return result
+    monkeypatch.setattr(requests, 'get', mock_get)
+    task.work()
+    assert os.path.exists(task.results["output"]) is True
+
+
