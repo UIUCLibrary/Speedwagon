b6a66c9d59348e78d0f4d98579b7a96811dbd164
henry <henryborchers@yahoo.com>
2020-12-18 16:38:56 -0600

    test_all_required_workflow_keys

--- speedwagon/job.py
+++ speedwagon/job.py
@@ -6,7 +6,7 @@ import inspect
 import logging
 import os
 import sys
-from typing import Type, Optional, Iterable, Dict, List, Any, Tuple
+from typing import Type, Optional, Iterable, Dict, List, Any, Tuple, Set
 
 from . import tasks
 from PyQt5 import QtWidgets  # type: ignore
@@ -21,6 +21,7 @@ class AbsWorkflow(metaclass=abc.ABCMeta):
     description: Optional[str] = None
     name: Optional[str] = None
     global_settings: Dict[str, str] = dict()
+    required_settings_keys: Set[str] = set()
 
     def __init__(self) -> None:
         super().__init__()
@@ -219,3 +220,11 @@ def available_workflows() -> dict:
     root = os.path.join(os.path.dirname(__file__), "workflows")
     finder = WorkflowFinder(root)
     return finder.locate()
+
+
+def all_required_workflow_keys() -> Set[str]:
+    workflows = available_workflows()
+    keys: Set[str] = set()
+    for workflow in workflows.values():
+        keys = keys.union(set(workflow.required_settings_keys))
+    return keys
---
+++ tests/test_jobs.py
@@ -0,0 +1,18 @@
+from unittest.mock import Mock
+
+import speedwagon.job
+
+
+def test_all_required_workflow_keys(monkeypatch):
+    def mocked_workflows():
+        return {
+            "spam": Mock(required_settings_keys={"spam_setting"}),
+            "bacon": Mock(required_settings_keys={"bacon_setting"}),
+            "eggs": Mock(required_settings_keys=set()),
+        }
+
+    monkeypatch.setattr(speedwagon.job, "available_workflows", mocked_workflows)
+
+    speedwagon.job.all_required_workflow_keys() == {
+        "spam_setting", "bacon_setting"
+    }
