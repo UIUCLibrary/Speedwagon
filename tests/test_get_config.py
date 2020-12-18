71db97ec882613cbd6aad598b04872784870dbea
henry <henryborchers@yahoo.com>
2020-12-18 14:14:03 -0600

    whitespace

--- tests/test_get_config.py
+++ tests/test_get_config.py
@@ -170,7 +170,7 @@ def test_windows_get_user_data_directory(monkeypatch):
 
 
 def test_generate_default_creates_file(tmpdir):
-    config_file = os.path.join(str(tmpdir),   "config.ini")
+    config_file = os.path.join(str(tmpdir), "config.ini")
     speedwagon.config.generate_default(str(config_file))
     assert os.path.exists(config_file)
 
