8ca35aa55025bea1e129188c404ac7ff9a48ee6c
henry <henryborchers@yahoo.com>
2020-12-21 16:28:57 -0600

    removed commented out code

--- speedwagon/tabs.py
+++ speedwagon/tabs.py
@@ -393,11 +393,6 @@ class WorkflowsTab(ItemSelectionTab):
         new_workflow = workflow(
             global_settings=dict(self.work_manager.user_settings)
         )
-        # new_workflow = workflow()
-
-        # new_workflow.global_settings.update(
-        #     dict(self.work_manager.user_settings))
-
         model = models.ToolOptionsModel3(new_workflow.user_options())
         return model
 
