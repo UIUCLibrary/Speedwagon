50ccbe9d33d4014a5b0224c5762ad70f46b3598b
henry <henryborchers@yahoo.com>
2020-12-21 16:48:03 -0600

    check that invalid tessdata path raises an expection

--- tests/workflows/test_ocr.py
+++ tests/workflows/test_ocr.py
@@ -1,9 +1,20 @@
 import pytest
-
+import os.path
 from speedwagon.workflows import workflow_ocr
-from speedwagon.exceptions import MissingConfiguration
+from speedwagon.exceptions import MissingConfiguration, SpeedwagonException
 
 
 def test_no_config():
     with pytest.raises(MissingConfiguration):
         workflow_ocr.OCRWorkflow()
+
+
+def test_discover_task_metadata_raises_with_no_tessdata(monkeypatch):
+    user_options = {"tessdata": "/some/path"}
+    monkeypatch.setattr(os.path, "exists", lambda args: True)
+    workflow = workflow_ocr.OCRWorkflow(global_settings=user_options)
+
+    monkeypatch.setattr(os.path, "exists", lambda args: False)
+    with pytest.raises(SpeedwagonException):
+        user_options = {"tessdata": None}
+        workflow.discover_task_metadata([], None, **user_options)
