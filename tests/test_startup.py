740f64a8b712439cf3cad5f6c22990f53e7069af
henry <henryborchers@yahoo.com>
2021-01-12 17:53:46 -0600

    mock out user.yaml

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -16,11 +16,14 @@ def test_version_exits_after_being_called(monkeypatch):
     version_exit_mock.assert_called()
 
 
-def test_run_loads_window(qtbot, monkeypatch):
+def test_run_loads_window(qtbot, monkeypatch, tmpdir):
     app = Mock()
     app.exec_ = MagicMock()
     standard_startup = startup.StartupDefault(app=app)
     standard_startup.startup_settings['debug'] = True
+    tabs_file = tmpdir / "user.yaml"
+    tabs_file.ensure()
+    standard_startup.tabs_file = tabs_file
 
     standard_startup.run()
     assert app.exec_.called is True
