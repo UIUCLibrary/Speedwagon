c1748fb4a4bb733c7d30a0e7c1c5ad7739b5aef0
henry <henryborchers@yahoo.com>
2021-01-13 09:49:05 -0600

    test about dialog with and without package metadata available

--- tests/test_dialog.py
+++ tests/test_dialog.py
@@ -1,11 +1,33 @@
 from PyQt5 import QtWidgets
+try:
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata  # type: ignore
 
 from speedwagon.dialog import dialogs
 
 
 def test_about_dialog_box(qtbot, monkeypatch):
+    fake_metadata = {
+        "Summary": "A collection of tools",
+        "Version": "1.1"
+    }
+
     def mock_about(parent, title, message):
-        assert "Speedwagon" in message
+        assert fake_metadata['Version'] in message and fake_metadata['Summary'] in message
+
+    def mock_metadata(*args, **kwargs):
+        return fake_metadata
+
+    with monkeypatch.context() as mp:
+        mp.setattr(QtWidgets.QMessageBox, "about", mock_about)
+        mp.setattr(metadata, "metadata", mock_metadata)
+        dialogs.about_dialog_box(None)
+
+
+def test_about_dialog_box_no_metadata(qtbot, monkeypatch):
+    def mock_about(parent, title, message):
+        assert 'Speedwagon' == message
 
     with monkeypatch.context() as mp:
         mp.setattr(QtWidgets.QMessageBox, "about", mock_about)
