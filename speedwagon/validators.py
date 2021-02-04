c1f786d1ea471735ea3a2bf3b81330d90596317f
henry <henryborchers@yahoo.com>
2021-02-04 15:59:54 -0600

    type hints

--- speedwagon/validators.py
+++ speedwagon/validators.py
@@ -1,15 +1,15 @@
 import abc
 import os
-from typing import Dict
+from typing import Dict, Mapping, Any, Optional
 
 
 class AbsOptionValidator(abc.ABC):
     @abc.abstractmethod
-    def is_valid(self, **user_data) -> bool:
+    def is_valid(self, **user_data: Mapping[str, Any]) -> bool:
         """Evaluate if the kwargs are valid"""
 
     @abc.abstractmethod
-    def explanation(self, **user_data) -> str:
+    def explanation(self, **user_data: Mapping[str, Any]) -> str:
         """Get reason for is_valid.
 
         Args:
@@ -30,17 +30,23 @@ class DirectoryValidation(AbsOptionValidator):
     def destination_exists(path: str) -> bool:
         return os.path.exists(path)
 
-    def is_valid(self, **user_data) -> bool:
-        output = user_data.get(self._key)
-        if not output:
+    def is_valid(self, **user_data: Mapping[str, Any]) -> bool:
+        if self._key not in user_data:
+            return False
+        output = user_data[self._key]
+        if not isinstance(output, str):
             return False
         if self.destination_exists(output) is False:
             return False
         return True
 
-    def explanation(self, **user_data) -> str:
-        if self.destination_exists(user_data[self._key]) is False:
-            return f"Directory {user_data[self._key]} does not exist"
+    def explanation(self, **user_data: Mapping[str, Any]) -> str:
+        destination = user_data[self._key]
+        if not isinstance(destination, str):
+            raise TypeError(f"{self._key} not a string")
+
+        if self.destination_exists(destination) is False:
+            return f"Directory {destination} does not exist"
         return "ok"
 
 
