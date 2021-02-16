12432dcf9318aa065af95a7bb17b83bb5d09c541
henryborchers@yahoo.com <henryborchers@yahoo.com>
2021-02-15 23:43:53 -0600

    pep8

--- speedwagon/job.py
+++ speedwagon/job.py
@@ -57,7 +57,11 @@ class AbsWorkflow(metaclass=abc.ABCMeta):
             need a user's interaction.
         """
 
-    def create_new_task(self, task_builder: tasks.TaskBuilder, **job_args) -> None:
+    def create_new_task(
+            self,
+            task_builder: tasks.TaskBuilder,
+            **job_args
+    ) -> None:
         """Add a new task to be accomplished when the workflow is started.
 
 
