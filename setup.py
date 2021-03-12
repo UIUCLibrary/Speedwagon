b7f79db0611b08bc1ee8cf9738571bfa54cac66d
henry <henryborchers@yahoo.com>
2021-03-12 14:32:47 -0600

    uiucprescon.packager==0.2.11b8

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
             "pyyaml",
             "uiucprescon.imagevalidate>=0.1.5",
             "uiucprescon.ocr>=0.1.1",
-            "uiucprescon.packager[kdu]>=0.2.11b7",
+            "uiucprescon.packager[kdu]>=0.2.11b8",
             "uiucprescon.images",
             'uiucprescon-getmarc',  # TODO: get rid of this requirement
             "pykdu-compress>=0.1.3",
