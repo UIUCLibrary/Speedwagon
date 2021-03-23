f5afa2a503cbda4015380325432fcfcdac8cd851
henry <henryborchers@yahoo.com>
2021-03-23 12:22:46 -0500

    test_validate_yml_task_calls_validator

--- tests/workflows/test_completeness.py
+++ tests/workflows/test_completeness.py
@@ -214,3 +214,25 @@ def test_validate_marc_task_calls_validator(monkeypatch):
                mock_run_validation.call_args[0][0],
                validator.ValidateMarc
            )
+
+def test_validate_yml_task_calls_validator(monkeypatch):
+    package_path = os.path.join("sample_path", "package1")
+    expected_yaml_file = os.path.join(package_path, "meta.yml")
+    from hathi_validate import process, validator
+
+    task = workflow_completeness.ValidateYMLTask(package_path=package_path)
+    task.log = Mock()
+    mock_run_validation = MagicMock(return_value=[])
+
+    with monkeypatch.context() as mp:
+        mp.setattr(process, "run_validation", mock_run_validation)
+        mp.setattr(os.path, "exists", lambda x: x == expected_yaml_file)
+        assert task.work() is True
+
+    assert mock_run_validation.called is True and \
+           mock_run_validation.call_args[0][0].path == package_path and \
+           mock_run_validation.call_args[0][0].yaml_file == expected_yaml_file and \
+           isinstance(
+               mock_run_validation.call_args[0][0],
+               validator.ValidateMetaYML
+           )
