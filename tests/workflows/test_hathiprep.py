cbc9000da272962f91dba9246009527471df16dc
henry <henryborchers@yahoo.com>
2021-03-25 12:57:16 -0500

    test_discover_task_metadata_one_per_package

--- tests/workflows/test_hathiprep.py
+++ tests/workflows/test_hathiprep.py
@@ -1,4 +1,4 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, MagicMock
 
 import pytest
 from speedwagon.workflows import workflow_hathiprep
@@ -30,4 +30,31 @@ def test_initial_task_creates_task():
     assert \
         mock_builder.add_subtask.called is True and \
         mock_builder.add_subtask.call_args_list[0][0][0]._root == user_args['input']
+
+
+@pytest.fixture
+def unconfigured_workflow():
+    workflow = workflow = workflow_hathiprep.HathiPrepWorkflow()
+    user_options = {i.label_text: i.data for i in workflow.user_options()}
+
+    return workflow, user_options
+
+
+def test_discover_task_metadata_one_per_package(
+        monkeypatch, unconfigured_workflow):
+
+    workflow, user_options = unconfigured_workflow
+    number_of_fake_packages = 10
+
+    initial_results = []
+    additional_data = {
+        "packages": [MagicMock() for _ in range(number_of_fake_packages)]
+    }
+
+    new_task_md = workflow.discover_task_metadata(
+        initial_results=initial_results,
+        additional_data=additional_data,
+        **user_options
+    )
+    assert len(new_task_md) == number_of_fake_packages
 
