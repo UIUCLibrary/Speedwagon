9005c8de000c03274b21955e926c79f693f03991
henry <henryborchers@yahoo.com>
2021-03-25 13:31:42 -0500

    test_generate_checksum_task_task_calls_create_checksum_report

--- tests/workflows/test_hathiprep.py
+++ tests/workflows/test_hathiprep.py
@@ -136,3 +136,28 @@ def test_make_yaml_task_calls_make_yaml(monkeypatch):
         mp.setattr(shutil, "move", lambda source, dest: True)
         assert task.work() is True
     assert mock_package_builder.make_yaml.called is True
+
+
+def test_generate_checksum_task_task_calls_create_checksum_report(monkeypatch):
+    root_path = "some/sample/root"
+
+    task = workflow_hathiprep.GenerateChecksumTask(
+        package_id="1234",
+        source=root_path,
+    )
+
+    task.log = Mock()
+    mock_package_builder = MagicMock()
+
+    def mock_inplace_package(*args, **kwargs):
+        return mock_package_builder
+
+    from pyhathiprep import package_creater
+    import shutil
+    with monkeypatch.context() as mp:
+        mp.setattr(package_creater, "InplacePackage", mock_inplace_package)
+        mp.setattr(os, "makedirs", lambda x: None)
+        mp.setattr(os.path, "exists", lambda x: True)
+        mp.setattr(shutil, "move", lambda source, dest: True)
+        assert task.work() is True
+    assert mock_package_builder.create_checksum_report.called is True
