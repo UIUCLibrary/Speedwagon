7e17d503efaa1e5ed31233893ba35b1987f03443
henry <henryborchers@yahoo.com>
2021-03-25 12:48:16 -0500

    test_initial_task_creates_task

--- tests/workflows/test_hathiprep.py
+++ tests/workflows/test_hathiprep.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 import pytest
 from speedwagon.workflows import workflow_hathiprep
 
@@ -11,4 +13,21 @@ def test_workflow_options(index, label):
     user_options = workflow.user_options()
     assert len(user_options) > 0
     assert user_options[index].label_text == label
+
+
+def test_initial_task_creates_task():
+    workflow = workflow_hathiprep.HathiPrepWorkflow()
+    user_args = {
+        "input": "./some_real_source_folder",
+        "Image File Type": "JPEG 2000",
+    }
+
+    mock_builder = Mock()
+    workflow.initial_task(
+        task_builder=mock_builder,
+        **user_args
+    )
+    assert \
+        mock_builder.add_subtask.called is True and \
+        mock_builder.add_subtask.call_args_list[0][0][0]._root == user_args['input']
 
