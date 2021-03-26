a2362a62d070c24597cf359b9d4060e5b23ab74c
henry <henryborchers@yahoo.com>
2021-03-26 12:11:11 -0500

    create tests for workflow_convertCaptureOnePreservationToDigitalLibJP2

---
+++ tests/workflows/test_convert_capture_one_preservation_to_digital_lib_jp2.py
@@ -0,0 +1,38 @@
+from unittest.mock import Mock
+
+from speedwagon.workflows import \
+    workflow_convertCaptureOnePreservationToDigitalLibJP2 as \
+        capture_one_workflow
+
+
+def test_package_image_task_success(monkeypatch):
+    mock_processfile = Mock()
+    with monkeypatch.context() as mp:
+        import os
+        mp.setattr(capture_one_workflow, "ProcessFile", mock_processfile)
+        mp.setattr(os, "makedirs", lambda *x: None)
+        task = capture_one_workflow.PackageImageConverterTask(
+            source_file_path="spam",
+            dest_path="eggs"
+        )
+        assert task.work() is True
+    assert mock_processfile.called is True
+
+
+def test_package_image_task_failure(monkeypatch):
+    import os
+    mock_processfile = Mock()
+    mock_processfile.process = Mock(side_effect=capture_one_workflow.ProcessingException("failure"))
+
+    def get_mock_processfile(*args):
+        return mock_processfile
+
+    with monkeypatch.context() as mp:
+        mp.setattr(os, "makedirs", lambda *x: None)
+        mp.setattr(capture_one_workflow, "ProcessFile", get_mock_processfile)
+        task = capture_one_workflow.PackageImageConverterTask(
+            source_file_path="spam",
+            dest_path="eggs"
+        )
+        assert task.work() is False
+
