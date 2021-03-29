14c184ad441d8378ad1bc04a59abf647aa9d2e9f
henry <henryborchers@yahoo.com>
2021-03-29 10:25:59 -0500

    test read_tabs_yaml and write_tabs_yaml

--- tests/test_tabs.py
+++ tests/test_tabs.py
@@ -1,8 +1,9 @@
 from collections import OrderedDict
-from unittest.mock import Mock, MagicMock
+from unittest import mock
+from unittest.mock import Mock, MagicMock, patch, call
 
 import pytest
-
+import yaml
 from speedwagon import tabs, exceptions, job
 
 
@@ -133,3 +134,45 @@ class TestWorkflowsTab:
             assert isinstance(mock_runner.call_args_list[0][0][1], MockWorkflow)
 
         assert mock_message_box_exec.called is True
+
+
+class TestTabsYaml:
+    @pytest.mark.parametrize("exception_type", [
+        FileNotFoundError,
+        AttributeError,
+        yaml.YAMLError,
+    ])
+    def test_read_tabs_yaml_errors(self, monkeypatch,
+                                   exception_type):
+        with patch('speedwagon.tabs.open', mock.mock_open()):
+            import yaml
+            monkeypatch.setattr(yaml, "load", Mock(side_effect=exception_type))
+            with pytest.raises(exception_type) as e:
+                list(tabs.read_tabs_yaml('tabs.yml'))
+            assert e.type == exception_type
+
+    def test_read_tabs_yaml(self, monkeypatch):
+        sample_text = """my stuff:
+- Convert CaptureOne Preservation TIFF to Digital Library Access JP2
+- Convert CaptureOne TIFF to Digital Library Compound Object
+- Convert CaptureOne TIFF to Digital Library Compound Object and HathiTrust
+- Convert CaptureOne TIFF to Hathi TIFF package
+        """
+        import os
+        monkeypatch.setattr(os.path, 'getsize', lambda x: 1)
+        with patch('speedwagon.tabs.open',
+                   mock.mock_open(read_data=sample_text)):
+
+            tab_data = list(tabs.read_tabs_yaml('tabs.yml'))
+            assert len(tab_data) == 1 and \
+                   len(tab_data[0][1].workflows) == 4
+
+    def test_write_tabs_yaml(self, ):
+        sample_tab_data = [
+            tabs.TabData("dummy_tab", MagicMock())
+        ]
+        with patch('speedwagon.tabs.open', mock.mock_open()) as m:
+            tabs.write_tabs_yaml("tabs.yml", sample_tab_data)
+            assert m.called is True
+        handle = m()
+        handle.write.assert_has_calls([call('dummy_tab')])
