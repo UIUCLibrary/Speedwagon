24a39cc5fc21bca2087514f91639684d7934a8ed
henry <henryborchers@yahoo.com>
2021-03-28 20:28:45 -0500

    test_init_selects_first_workflow

--- tests/test_tabs.py
+++ tests/test_tabs.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from unittest.mock import Mock, MagicMock
 
 import pytest
@@ -28,3 +29,42 @@ class TestWorkflowsTab:
             selection_tab.item_selected(index)
         assert str(e.value) == "something wrong happened" and \
                mock_message_box_exec.called is True
+
+    @pytest.mark.parametrize(
+        "number_of_indexes_selected, is_validate",
+        [
+            (0, False),
+            (1, True),
+            (2, False),
+        ]
+    )
+    def test_is_ready_to_start(self, qtbot, number_of_indexes_selected, is_validate):
+        log_manager = Mock()
+        work_manager = Mock()
+        selection_tab = tabs.WorkflowsTab(
+            parent=None,
+            workflows=MagicMock(),
+            log_manager=log_manager,
+            work_manager=work_manager
+        )
+        selection_tab.item_selector_view.selectedIndexes = \
+            Mock(return_value=[Mock() for _ in range(number_of_indexes_selected)])
+        assert selection_tab.is_ready_to_start() is is_validate
+
+    def test_init_selects_first_workflow(self, qtbot):
+        log_manager = Mock()
+        work_manager = Mock(user_settings={})
+        workflows = OrderedDict()
+        workflows["Spam"] = MagicMock()
+        workflows["Bacon"] = MagicMock()
+
+        selection_tab = tabs.WorkflowsTab(
+            parent=None,
+            workflows=workflows,
+            log_manager=log_manager,
+            work_manager=work_manager
+        )
+        selection_tab.init_selection()
+        assert selection_tab.item_selector_view.currentIndex().data() == \
+               workflows['Spam'].name
+
