c98513d03ad4a5fb1974a43f2332ec3b88b6bdd5
henry <henryborchers@yahoo.com>
2021-03-31 18:15:36 -0500

    pep8

--- tests/workflows/test_validate_metadata.py
+++ tests/workflows/test_validate_metadata.py
@@ -1,11 +1,14 @@
 import pytest
 from speedwagon.workflows import workflow_validate_metadata
+
 options = [
     (0, "Input"),
     (1, "Profile")
 ]
+
+
 @pytest.mark.parametrize("index,label", options)
-def test_validate_metadata_Workflow_has_options(index, label):
+def test_validate_metadata_workflow_has_options(index, label):
     workflow = workflow_validate_metadata.ValidateMetadataWorkflow()
     user_options = workflow.user_options()
     assert len(user_options) > 0
