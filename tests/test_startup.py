c3297d64acf30735d232369363c4aae1416b22e8
henry <henryborchers@yahoo.com>
2021-04-13 14:49:40 -0500

    test get_custom_tabs

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -1,5 +1,5 @@
 import argparse
-from unittest.mock import Mock, MagicMock
+from unittest.mock import Mock, MagicMock, mock_open, patch
 
 import pytest
 
@@ -98,3 +98,73 @@ def test_load_module_self_test(monkeypatch):
     with pytest.raises(SystemExit):
         speedwagon.__main__.main(["_", "--pytest"])
     assert pytest_mock.main.called is True
+
+
+def test_get_custom_tabs_missing_file(capsys, monkeypatch):
+    from speedwagon import startup
+    all_workflows = {
+        "my workflow": Mock()
+    }
+    import os
+    monkeypatch.setattr(os.path, "exists", lambda x: False)
+    list(startup.get_custom_tabs(all_workflows, "not_a_real_file"))
+    captured = capsys.readouterr()
+    assert "file not found" in captured.err
+
+
+def test_get_custom_tabs_bad_data_raises_exception(monkeypatch):
+    from speedwagon import startup
+    from speedwagon.startup import FileFormatError
+    import os
+    test_file = "test.yml"
+    monkeypatch.setattr(os.path, "exists", lambda x: x == test_file)
+    with pytest.raises(FileFormatError):
+        with patch('speedwagon.startup.open', mock_open(
+                read_data='not valid yml data')) as m:
+            list(startup.get_custom_tabs({"my workflow": Mock()}, test_file))
+
+
+def test_missing_workflow(monkeypatch, capsys):
+    from speedwagon import startup
+    import os
+    test_file = "test.yml"
+    monkeypatch.setattr(os.path, "exists", lambda x: x == test_file)
+    import yaml
+
+    # These workflow are not valid
+    tabs_config_data = {
+        "my workflow": [
+            "spam",
+            "bacon",
+            "eggs"
+        ]
+    }
+    load = Mock(name="load", return_value=tabs_config_data)
+    load.__class__ = dict
+    monkeypatch.setattr(yaml, "load", load)
+    with patch('speedwagon.startup.open', mock_open()) as m:
+        list(startup.get_custom_tabs({"my workflow": Mock()}, test_file))
+    captured = capsys.readouterr()
+    assert "Unable to load" in captured.err
+
+
+def test_get_custom_tabs_loads_workflows_from_file(monkeypatch):
+    from speedwagon import startup
+    import os
+    test_file = "test.yml"
+    monkeypatch.setattr(os.path, "exists", lambda x: x == test_file)
+    import yaml
+    all_workflows = {
+        "spam": Mock(active=True)
+    }
+    tabs_config_data = {
+        "my workflow": [
+            "spam",
+        ]
+    }
+    load = Mock(name="load", return_value=tabs_config_data)
+    load.__class__ = dict
+    monkeypatch.setattr(yaml, "load", load)
+    with patch('speedwagon.startup.open', mock_open()) as m:
+        tab_name, workflows = next(startup.get_custom_tabs(all_workflows, test_file))
+    assert "my workflow" == tab_name and "spam" in workflows
