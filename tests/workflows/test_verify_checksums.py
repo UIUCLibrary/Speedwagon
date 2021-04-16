3b977d47f088d1eac132815c447f387d1fb6b3d2
henry <henryborchers@yahoo.com>
2021-04-16 12:42:51 -0500

    grouped tests Sensitivity Comparison into a class

--- tests/workflows/test_verify_checksums.py
+++ tests/workflows/test_verify_checksums.py
@@ -1,21 +1,25 @@
 from speedwagon.workflows import workflow_verify_checksums
 
+class TestSensitivityComparison:
+    def test_sensitive_comparison_valid(self):
+        standard_strategy = workflow_verify_checksums.CaseSensitiveComparison()
+        assert \
+            standard_strategy.compare("asdfasdfasdf", "asdfasdfasdf") is True
 
-def test_sensitive_comparison_valid():
-    standard_strategy = workflow_verify_checksums.CaseSensitiveComparison()
-    assert standard_strategy.compare("asdfasdfasdf", "asdfasdfasdf") is True
+    def test_sensitive_comparison_invalid(self):
+        standard_strategy = workflow_verify_checksums.CaseSensitiveComparison()
+        assert \
+            standard_strategy.compare("asdfasdfasdf", "ASDFASDFASDF") is False
 
+    def test_insensitive_comparison_valid(self):
+        case_insensitive_strategy = \
+            workflow_verify_checksums.CaseInsensitiveComparison()
 
-def test_sensitive_comparison_invalid():
-    standard_strategy = workflow_verify_checksums.CaseSensitiveComparison()
-    assert standard_strategy.compare("asdfasdfasdf", "ASDFASDFASDF") is False
+        assert case_insensitive_strategy.compare("asdfasdfasdf",
+                                                 "asdfasdfasdf") is True
 
-
-def test_insensitive_comparison_valid():
-    case_insensitive_strategy = workflow_verify_checksums.CaseInsensitiveComparison()
-    assert case_insensitive_strategy.compare("asdfasdfasdf", "asdfasdfasdf") is True
-
-
-def test_insensitive_comparison_invalid():
-    case_insensitive_strategy = workflow_verify_checksums.CaseInsensitiveComparison()
-    assert case_insensitive_strategy.compare("asdfasdfasdf", "ASDFASDFASDF") is True
+    def test_insensitive_comparison_invalid(self):
+        case_insensitive_strategy = \
+            workflow_verify_checksums.CaseInsensitiveComparison()
+        assert case_insensitive_strategy.compare("asdfasdfasdf",
+                                                 "ASDFASDFASDF") is True
