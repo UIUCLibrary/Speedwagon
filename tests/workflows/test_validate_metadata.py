ff465b7dbe3885c061f4550525fede1220aeb9a9
henry <henryborchers@yahoo.com>
2021-04-20 16:52:25 -0500

    test_initial_task, test_create_new_task, and test_discover_task_metadata

--- tests/workflows/test_validate_metadata.py
+++ tests/workflows/test_validate_metadata.py
@@ -1,6 +1,10 @@
+from unittest.mock import Mock
+
 import pytest
 from speedwagon.workflows import workflow_validate_metadata
-from speedwagon import models
+from speedwagon import models, tasks
+import os
+
 options = [
     (0, "Input"),
     (1, "Profile")
@@ -41,25 +45,93 @@ class TestValidateMetadataWorkflow:
             mp.setattr(os.path, "isdir", lambda x: x == user_options['Input'])
             assert workflow.validate_user_options(**user_options) is True
 
-    @pytest.mark.parametrize("exists,is_dir",[
-        (False, False),
-        (True, False),
-        (False, True),
+    @pytest.mark.parametrize("input_data,exists,is_dir",[
+        (os.path.join("some", "valid", "path"), False, False),
+        (os.path.join("some", "valid", "path"), True, False),
+        (os.path.join("some", "valid", "path"), False, True),
     ])
     def test_validate_user_options_invalid(
             self,
             monkeypatch,
             workflow,
             default_options,
+            input_data,
             exists,
             is_dir
     ):
         user_options = default_options.copy()
-        import os
-        user_options['Input'] = os.path.join("some", "valid", "path")
+        user_options['Input'] = input_data
 
         with monkeypatch.context() as mp:
             mp.setattr(os.path, "exists", lambda x: exists)
             mp.setattr(os.path, "isdir", lambda x: is_dir)
             with pytest.raises(ValueError) as e:
                 assert workflow.validate_user_options(**user_options) is True
+
+    def test_initial_task(self, monkeypatch, workflow, default_options):
+        user_args = default_options.copy()
+        user_args["Input"] = os.path.join("some", "valid", "path")
+        user_args['Profile'] = 'HathiTrust JPEG 2000'
+
+        task_builder = Mock()
+        LocateImagesTask = Mock()
+        monkeypatch.setattr(
+            workflow_validate_metadata,
+            "LocateImagesTask",
+            LocateImagesTask
+        )
+        workflow.initial_task(
+            task_builder=task_builder,
+            **user_args
+        )
+
+        assert task_builder.add_subtask.called is True
+
+        LocateImagesTask.assert_called_with(
+            user_args['Input'],
+            user_args['Profile']
+        )
+
+    def test_discover_task_metadata(self, workflow, default_options):
+        user_options = default_options.copy()
+        user_options["Input"] = os.path.join("some", "valid", "path")
+        user_options['Profile'] = 'HathiTrust JPEG 2000'
+
+        initial_results = [
+            tasks.Result(workflow_validate_metadata.LocateImagesTask, [
+                "spam.jp2"
+            ])
+        ]
+        additional_data = {}
+        tasks_generated = workflow.discover_task_metadata(
+            initial_results=initial_results,
+            additional_data=additional_data,
+            **user_options
+        )
+        JobValues = workflow_validate_metadata.JobValues
+        assert len(tasks_generated) == 1
+        assert tasks_generated[0] == {
+            JobValues.ITEM_FILENAME.value: "spam.jp2",
+            JobValues.PROFILE_NAME.value: user_options["Profile"]
+        }
+
+    def test_create_new_task(self, monkeypatch, workflow):
+        job_args = {
+            "filename": "somefile.jp2",
+            "profile_name": 'HathiTrust JPEG 2000'
+        }
+        task_builder = Mock()
+        ValidateImageMetadataTask = Mock()
+        monkeypatch.setattr(
+            workflow_validate_metadata,
+            "ValidateImageMetadataTask",
+            ValidateImageMetadataTask
+        )
+
+        workflow.create_new_task(task_builder, **job_args)
+
+        assert task_builder.add_subtask.called is True
+        ValidateImageMetadataTask.assert_called_with(
+            job_args['filename'],
+            job_args['profile_name']
+        )
