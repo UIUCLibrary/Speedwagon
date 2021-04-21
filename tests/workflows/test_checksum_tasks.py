16719f5b8a129c934cf7f5bee79f3a668d49d349
henry <henryborchers@yahoo.com>
2021-04-21 15:43:39 -0500

    TestMakeCheckSumReportTask

--- tests/workflows/test_checksum_tasks.py
+++ tests/workflows/test_checksum_tasks.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock, MagicMock, patch, mock_open
+
 from speedwagon.workflows import checksum_tasks
 
 
@@ -21,3 +23,18 @@ class TestMakeChecksumTask:
         assert task.work() is True and \
             task.results[checksum_tasks.ResultsValues.SOURCE_HASH] == \
             "164e5c004f7468f23605f571d9a19cf9"
+
+
+class TestMakeCheckSumReportTask:
+    def test_work(self):
+        output_filename = "output_filename"
+        checksum_calculations = [MagicMock()]
+        task = checksum_tasks.MakeCheckSumReportTask(
+            output_filename=output_filename,
+            checksum_calculations=checksum_calculations
+        )
+
+        m = mock_open()
+        with patch('speedwagon.workflows.checksum_tasks.open', m):
+            assert task.work() is True
+        assert m.called is True
