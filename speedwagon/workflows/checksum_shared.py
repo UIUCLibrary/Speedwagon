1b1ae1ba993c7c9451801bce934be35014bcc721
henry <henryborchers@yahoo.com>
2021-06-24 10:10:36 -0500

    checksum_shared.py doc strings

--- speedwagon/workflows/checksum_shared.py
+++ speedwagon/workflows/checksum_shared.py
@@ -1,7 +1,11 @@
+"""Shared data for checksum workflows."""
+
 import enum
 
 
 class ResultsValues(enum.Enum):
+    """Values for results for Checksum reports."""
+
     SOURCE_FILE = "source_filename"
     SOURCE_HASH = "checksum_hash"
     CHECKSUM_FILE = "checksum_file"
