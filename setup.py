f3599c2cf204941a4cfe6c859f74220547018bcd
henry <henryborchers@yahoo.com>
2021-06-29 16:59:43 -0500

    update py3exiv2bind to 0.1.7b1

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
             'typing-extensions;python_version<"3.8"',
             'lxml',
             "requests",
-            "py3exiv2bind>=0.1.5",
+            "py3exiv2bind>=0.1.7b1",
         ],
         packages=[
             "speedwagon",
