059374cc751ff29221d487b0fa4f4abd0e6f2a31
henry <henryborchers@yahoo.com>
2021-06-30 10:08:22 -0500

    test_abort_calls_callback inlining var

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -8,8 +8,7 @@ from speedwagon import worker
 
 class TestWorkRunnerExternal3:
     def test_abort_calls_callback(self, qtbot):
-        work_runner = worker.WorkRunnerExternal3(QtWidgets.QWidget())
-        with work_runner as r:
+        with worker.WorkRunnerExternal3(QtWidgets.QWidget()) as r:
             r.abort_callback = Mock()
             r.abort()
         assert r.abort_callback.called is True
