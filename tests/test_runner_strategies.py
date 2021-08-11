41b8f236fdc9bf4376649dee3228721c9754ad5b
henry <henryborchers@yahoo.com>
2021-08-11 17:42:20 -0500

    test QtDialogProgress properties

--- tests/test_runner_strategies.py
+++ tests/test_runner_strategies.py
@@ -366,3 +366,29 @@ class TestQtDialogProgress:
 
         assert dialog_box.dialog.value() == 0 and \
                dialog_box.dialog.maximum() == 0
+
+    def test_total_tasks_amount_affects_dialog(self, qtbot):
+        dialog_box = runner_strategies.QtDialogProgress()
+        dialog_box.total_tasks_amount = 10
+        assert dialog_box.dialog.maximum() == 10 and \
+               dialog_box.total_tasks_amount == 10
+
+    def test_current_tasks_progress_affects_dialog(self, qtbot):
+        dialog_box = runner_strategies.QtDialogProgress()
+        dialog_box.total_tasks_amount = 10
+        dialog_box.current_task_progress = 5
+        assert dialog_box.dialog.value() == 5 and \
+               dialog_box.current_task_progress == 5
+
+    def test_title_affects_dialog(self, qtbot):
+        dialog_box = runner_strategies.QtDialogProgress()
+        dialog_box.title = "spam"
+        assert dialog_box.dialog.windowTitle() == "spam" and \
+               dialog_box.title == "spam"
+
+    def test_details_affects_dialog(self, qtbot):
+        dialog_box = runner_strategies.QtDialogProgress()
+        dialog_box.details = "spam"
+        assert dialog_box.dialog.labelText() == "spam" and \
+               dialog_box.details == "spam"
+
