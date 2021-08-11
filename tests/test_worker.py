47c932d2002825c1bc43cb055c480c3073f5a807
henry <henryborchers@yahoo.com>
2021-08-11 15:12:23 -0500

    removed unused buffered ProgressMessageBoxLogHandler

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -26,37 +26,3 @@ class TestWorkRunnerExternal3:
                 r.dialog = None
         assert "dialog" in str(e.value)
 
-
-class TestProgressMessageBoxLogHandler:
-    def test_flush_first_time(self):
-        handler = worker.ProgressMessageBoxLogHandler()
-        checker = worker.NeverBeenFlushed(handler)
-        assert checker.should_be_flush() is True
-
-    def test_flush_if_time_greater_than_refresh_rate(self):
-        handler = worker.ProgressMessageBoxLogHandler()
-        checker = worker.FlushWaitedLongEnough(handler)
-
-        handler.last_flushed_time = 0
-        assert checker.should_be_flush() is True
-
-    def test_no_flush_if_timer_thread_already_set(self):
-        handler = worker.ProgressMessageBoxLogHandler()
-        handler.update_thread = Mock(name="Mock thread")
-        checker = worker.TimerThreadAlreadyRunning(handler)
-        assert checker.should_be_flush() is False
-
-    @pytest.mark.parametrize("checker_value, expected", [
-        (False, False),
-        (True, True),
-        (None, False),
-    ]
-                             )
-    def test_ready_to_flush_checks_return_value(self, checker_value, expected):
-        handler = worker.ProgressMessageBoxLogHandler()
-        mock_checker = Mock()
-        mock_checker.should_be_flush.return_value = checker_value
-        checks = [
-            mock_checker
-        ]
-        assert handler.ready_to_flush(checks=checks) is expected
