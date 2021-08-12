caa7608583f2239cfa34f2851fb7d88023f67942
henry <henryborchers@yahoo.com>
2021-08-12 12:53:12 -0500

    test_show_system_info

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -125,15 +125,44 @@ def test_set_current_tab(qtbot):
 
 
 class TestMainWindow:
+    def test_about_dialog_box(self, qtbot, monkeypatch):
+        work_manager = Mock()
+        work_manager.settings_path = None
+        window = speedwagon.gui.MainWindow(work_manager)
+        qtbot.add_widget(window)
+        about_dialog_box = Mock()
+        monkeypatch.setattr(
+            speedwagon.dialog.dialogs,
+            "about_dialog_box",
+            about_dialog_box
+        )
+        window.show_about_window()
+        assert about_dialog_box.called is True
+
+    def test_show_system_info(self, qtbot, monkeypatch):
+        work_manager = Mock()
+        work_manager.settings_path = None
+        window = speedwagon.gui.MainWindow(work_manager)
+        qtbot.add_widget(window)
+        exec_ = Mock()
+        monkeypatch.setattr(
+            speedwagon.dialog.dialogs.SystemInfoDialog,
+            "exec",
+            exec_
+        )
+        window.show_system_info()
+        assert exec_.called is True
+
     def test_show_configuration_opens_settings_dialog(self, qtbot, monkeypatch):
         work_manager = Mock()
         work_manager.settings_path = None
-        mw = speedwagon.gui.MainWindow(work_manager)
+        window = speedwagon.gui.MainWindow(work_manager)
+        qtbot.add_widget(window)
         exec_ = Mock()
         monkeypatch.setattr(
             speedwagon.dialog.settings.SettingsDialog,
             "exec",
             exec_
         )
-        mw.show_configuration()
+        window.show_configuration()
         assert exec_.called is True
