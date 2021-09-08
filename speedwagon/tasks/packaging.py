e88439924053356eb654999ff10fd8b31ed1b565
henry <henryborchers@yahoo.com>
2021-09-08 12:05:41 -0500

    documentation for packaging.py

--- speedwagon/tasks/packaging.py
+++ speedwagon/tasks/packaging.py
@@ -1,19 +1,37 @@
+"""Tasks related to file packages.
+
+Created on 9/8/2021 by Henry Borchers
+
+"""
+
 import abc
 from typing import Optional
 import speedwagon
 
 
 class AbsFindPackageTask(speedwagon.tasks.Subtask, abc.ABC):
+    """Base class for creating find package tasks.
+
+    To implement, override the find_packages method.
+    """
+
     name = "Locating Packages"
 
     def __init__(self, root: str) -> None:
+        """Create a new find package tasks that searches at a given location.
+
+        Args:
+            root: Path to search for packages
+        """
         super().__init__()
         self._root = root
 
     def task_description(self) -> Optional[str]:
+        """Describe where the packages are being searched for."""
         return f"Locating packages in {self._root}"
 
     def work(self) -> bool:
+        """Perform the task."""
         self.log("Locating packages in {}".format(self._root))
         self.set_results(self.find_packages(self._root))
         return True
@@ -26,4 +44,5 @@ class AbsFindPackageTask(speedwagon.tasks.Subtask, abc.ABC):
             search_path:
 
         Returns:
+            Returns packages located.
         """
