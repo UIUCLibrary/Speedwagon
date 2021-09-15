a31c3599d1e7035336af13a8109b1b0fc7ccc4e8
Henry Borchers <hborcher@illinois.edu>
2021-09-15 15:41:44 -0500

    Fix choco version (#342)

--- ci/jenkins/scripts/chocolatey.groovy
+++ ci/jenkins/scripts/chocolatey.groovy
@@ -4,17 +4,28 @@ def sanitize_chocolatey_version(version){
 
 //        def rc_pattern = "(?<=\d(\.?))rc((?=\d)?)"
         def dashed_version = version.replaceFirst(dot_to_slash_pattern, "-")
+        if ( version =~ /dev/ ) {
+            return version.replace('.dev', "-dev")
+        }
         dashed_version = version.replaceFirst('\\.post', ".")
+        def dev_pattern = '(?<=\\d(\\.?))dev((?=\\d)?)'
+        if(dashed_version.matches(dev_pattern)){
+            echo "Discovered a development version"
+            return dashed_version.replaceFirst(dev_pattern, "-dev")
+        }
 
-        def beta_pattern = "(?<=\\d(\\.?))b((?=\\d)?)"
+        def beta_pattern = '(?<=\\d(\\.?))b((?=\\d)?)'
         if(dashed_version.matches(beta_pattern)){
+            echo "Discovered a beta version"
             return dashed_version.replaceFirst(beta_pattern, "beta")
         }
 
-        def alpha_pattern = "(?<=\\d(\\.?))a((?=\\d)?)"
+        def alpha_pattern = '(?<=\\d(\\.?))a((?=\\d)?)'
         if(dashed_version.matches(alpha_pattern)){
+            echo "Discovered an Alpha version"
             return dashed_version.replaceFirst(alpha_pattern, "alpha")
         }
+        echo "Discovered no special version info"
         return dashed_version
     }
 }
