b11ed1504e1fa09a43f81455e972d9428549dd69
henry <henryborchers@yahoo.com>
2021-11-18 15:38:50 -0600

    install visual studio 17

--- ci/docker/windows_standalone/startup.bat
+++ ci/docker/windows_standalone/startup.bat
@@ -1,4 +1,5 @@
 @echo off
+echo here
 if not defined DevEnvDir (
-    CALL C:\BuildTools\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64
+    CALL "c:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat" "-arch=amd64 -host_arch=amd64"
     )
