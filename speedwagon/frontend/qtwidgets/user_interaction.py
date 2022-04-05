50722cb016d8edba8dcb4e10bda73e1750273136
henry <henryborchers@yahoo.com>
2022-04-05 09:51:44 -0500

    test_get_data_with_dialog_box

--- speedwagon/frontend/qtwidgets/user_interaction.py
+++ speedwagon/frontend/qtwidgets/user_interaction.py
@@ -1,4 +1,5 @@
 """User interaction when using a QtWidget backend."""
+import typing
 from typing import Dict, Any, Optional, List
 from PySide6 import QtWidgets
 from uiucprescon.packager.packages import collection
@@ -54,10 +55,11 @@ class QtWidgetPackageBrowserWidget(interaction.AbstractPackageBrowser):
     def get_data_with_dialog_box(
             self,
             root_dir: str,
-            image_type: interaction.SupportedImagePackageFormats
+            image_type: interaction.SupportedImagePackageFormats,
+            dialog_box: typing.Type[PackageBrowser] = PackageBrowser
     ) -> List[collection.Package]:
         """Open a Qt dialog box for selecting package title pages."""
-        browser = PackageBrowser(
+        browser = dialog_box(
             self.get_packages(root_dir, image_type),
             self.parent
         )
--- tests/frontend/test_qtwidgets.py
+++ tests/frontend/test_qtwidgets.py
@@ -1,6 +1,7 @@
 from unittest.mock import MagicMock, Mock
 
-from speedwagon.frontend import qtwidgets
+from speedwagon.frontend import qtwidgets, interaction
+from speedwagon.workflows import title_page_selection
 
 
 class TestQtWidgetPackageBrowserWidget:
@@ -41,3 +42,24 @@ class TestQtWidgetPackageBrowserWidget:
         assert response == {
             "packages": packages
         }
+
+    def test_get_data_with_dialog_box(self, monkeypatch):
+        package_widget = \
+            qtwidgets.user_interaction.QtWidgetPackageBrowserWidget(None)
+
+        mock_dialog_box = Mock(spec=title_page_selection.PackageBrowser)
+
+        mock_dialog_box_type = Mock(return_value=mock_dialog_box)
+
+        monkeypatch.setattr(
+            package_widget,
+            "get_packages",
+            lambda root_dir, image_type: [Mock()]
+        )
+        package_widget.get_data_with_dialog_box(
+            root_dir="somePath",
+            image_type=interaction.SupportedImagePackageFormats.JP2,
+            dialog_box=mock_dialog_box_type
+        )
+        assert mock_dialog_box_type.called is True and \
+               mock_dialog_box.exec.called is True
