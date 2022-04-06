4f1d5e00b3a6244420e664b297039a8b56133596
henry <henryborchers@yahoo.com>
2022-04-06 12:16:34 -0500

    docstrings

--- speedwagon/tasks/filesystem.py
+++ speedwagon/tasks/filesystem.py
@@ -1,3 +1,5 @@
+"""Tasks related to doing something with files on a file system."""
+
 import abc
 import os
 from typing import Optional
@@ -6,8 +8,14 @@ import speedwagon
 
 
 class DeleteFileSystemItem(speedwagon.tasks.Subtask, abc.ABC):
+    """Base class for removing an item from a file system."""
 
     def __init__(self, path: str) -> None:
+        """Create a new task for removing an item from the file system.
+
+        Args:
+            path: path to file or directory.
+        """
         super().__init__()
         self.path = path
 
@@ -16,6 +24,7 @@ class DeleteFileSystemItem(speedwagon.tasks.Subtask, abc.ABC):
         """Remove the item."""
 
     def work(self) -> bool:
+        """Run the task."""
         self.log(f"Removing {self.path}")
         self.remove()
         self.set_results(self.path)
@@ -23,14 +32,18 @@ class DeleteFileSystemItem(speedwagon.tasks.Subtask, abc.ABC):
 
 
 class DeleteFile(DeleteFileSystemItem):
+    """Remove a file from the file system."""
 
     def task_description(self) -> Optional[str]:
+        """Explain what os being removed."""
         return "Deleting file"
 
     def remove(self) -> None:
+        """Remove the file in the path attribute."""
         os.remove(self.path)
 
     def work(self) -> bool:
+        """Run the task."""
         self.log(f"Deleting {self.path}")
         self.remove()
         self.set_results(self.path)
@@ -38,9 +51,12 @@ class DeleteFile(DeleteFileSystemItem):
 
 
 class DeleteDirectory(DeleteFileSystemItem):
+    """Remove a directory from the file system."""
 
     def remove(self) -> None:
+        """Remove item in path attribute."""
         os.rmdir(self.path)
 
     def task_description(self) -> Optional[str]:
+        """Explain what os being removed."""
         return "Removing directory"
