b6543f58ddf2ad26a5643f74d2fb9522d1eb55c5
henry <henryborchers@yahoo.com>
2022-04-08 15:52:44 -0500

    hoist cli symbol up

--- speedwagon/frontend/__init__.py
+++ speedwagon/frontend/__init__.py
@@ -2,9 +2,10 @@
 from . import reporter
 from . import interaction
 from . import qtwidgets
+from . import cli
 __all__ = [
     'reporter',
     "interaction",
-    "qtwidgets"
-
+    "qtwidgets",
+    "cli"
 ]
