4b5f9d1e685a8e924d7fc08f9e819bffa887dcd0
henry <henryborchers@yahoo.com>
2022-04-11 11:18:50 -0500

    docstrings

--- speedwagon/runner.py
+++ speedwagon/runner.py
@@ -1,12 +1,15 @@
+"""Job runners."""
+
 import abc
 import logging
 import speedwagon
 
 
 class AbsRunner2(metaclass=abc.ABCMeta):
+    """Abstract class for creating runners."""
 
     @abc.abstractmethod
     def run(self, job: speedwagon.job.AbsWorkflow,
             options: dict,
             logger: logging.Logger, completion_callback=None) -> None:
-        pass
+        """Run the workflow."""
