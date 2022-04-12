c3657a9fb7e2a67052606ca1eb0e772bc7ee04da
henry <henryborchers@yahoo.com>
2022-04-12 13:29:08 -0500

    include all speedwagon

--- packaging/hook-speedwagon.py
+++ packaging/hook-speedwagon.py
@@ -1,2 +1,3 @@
-from PyInstaller.utils.hooks import copy_metadata
-datas = copy_metadata('speedwagon', recursive=True)
+from PyInstaller.utils.hooks import copy_metadata, collect_all
+datas, binaries, hiddenimports = collect_all('speedwagon')
+datas += copy_metadata('speedwagon', recursive=True)
