21a5d0ec0e39c43bee5d95d377a6b7045e17b4e5
henry <henryborchers@yahoo.com>
2022-05-12 16:43:12 -0500

    update docstring

--- speedwagon/utils.py
+++ speedwagon/utils.py
@@ -1,15 +1,19 @@
-from typing import Iterator
+from typing import Iterator, Callable
 
 import logging
 from contextlib import contextmanager
 
 
 @contextmanager
-def log_config(logger: logging.Logger, callback) -> Iterator[None]:
+def log_config(
+        logger: logging.Logger,
+        callback: Callable[[str], None]
+) -> Iterator[None]:
     """Configure logs so they get forwarded to the speedwagon console.
 
     Args:
         logger:
+        callback:
 
     """
     try:
