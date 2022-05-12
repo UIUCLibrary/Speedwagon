55121e5c6569f519803deef5652410d585a1f32f
henry <henryborchers@yahoo.com>
2022-05-12 14:43:29 -0500

    double quotes

--- ci/docker/chocolatey_package/choco_package_template/pythonscript/tools/chocolateyInstall.ps1
+++ ci/docker/chocolatey_package/choco_package_template/pythonscript/tools/chocolateyInstall.ps1
@@ -16,7 +16,7 @@ $PYTHON = "$(Join-Path $(Get-AppInstallLocation('python')) 'python.exe')"
 Write-Host "Creating Python virtualenv at $installDir\venv"
 & "$PYTHON" -m venv $installDir\venv
 & "$installDir\venv\Scripts\python.exe" -m pip install pip --upgrade --no-compile
-& "$installDir\venv\Scripts\python.exe" -m pip install  ''$($fileLocation)[QT]'' --find-link "$depDendenciesLocation"
+& "$installDir\venv\Scripts\python.exe" -m pip install  ''$fileLocation[QT]'' --find-link "$depDendenciesLocation"
 
 $files = get-childitem $installDir -include *.exe -recurse
 foreach ($file in $files) {
