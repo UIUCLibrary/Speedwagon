0c877d02f197341eb9e0c5a856f5e26916a8fd6e
henry <henryborchers@yahoo.com>
2023-06-09 17:10:24 -0500

    monkeypatch the correct module

--- tests/test_get_config.py
+++ tests/test_get_config.py
@@ -229,7 +229,7 @@ def test_add_empty_keys_if_missing(tmpdir, monkeypatch):
     config_file = str(os.path.join(tmpdir, "config.ini"))
     with monkeypatch.context() as mp:
         mp.setattr(
-            speedwagon.config, "get_platform_settings", lambda: {'user_data_directory': tmpdir.strpath}
+            speedwagon.config.config, "get_platform_settings", lambda: {'user_data_directory': tmpdir.strpath}
         )
         speedwagon.config.config.generate_default(config_file)
     keys_that_dont_exist = {"spam", "bacon"}
