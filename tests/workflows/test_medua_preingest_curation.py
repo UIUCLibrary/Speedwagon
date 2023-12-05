b595058594a4b4512f58830787e4291a613436d4
henry <henryborchers@yahoo.com>
2023-12-05 16:39:59 -0600

    Speedwagon-488 Medusa Preingest Curation not properly deleting CaptureOne files

--- tests/workflows/test_medua_preingest_curation.py
+++ tests/workflows/test_medua_preingest_curation.py
@@ -13,9 +13,7 @@ import speedwagon.tasks
 class TestMedusaPreingestCuration:
     @pytest.fixture
     def default_args(self, workflow):
-        return {
-            data.label: data.value for data in workflow.job_options()
-        }
+        return {data.label: data.value for data in workflow.job_options()}
 
     @pytest.fixture
     def workflow(self):
@@ -26,8 +24,9 @@ class TestMedusaPreingestCuration:
             workflow.validate_user_options(**default_args)
 
     def test_valid_user_args_returns_true(self, workflow, default_args):
-        workflow_medusa_preingest\
-            .MedusaPreingestCuration.validation_checks = []
+        workflow_medusa_preingest.MedusaPreingestCuration.validation_checks = (
+            []
+        )
 
         assert workflow.validate_user_options(**default_args) is True
 
@@ -42,17 +41,16 @@ class TestMedusaPreingestCuration:
         data = [
             "./some/file.txt",
             "./some/directory/",
-
         ]
         monkeypatch.setattr(
             workflow_medusa_preingest.os.path,
             "isdir",
-            lambda path: path == "./some/directory/"
+            lambda path: path == "./some/directory/",
         )
         monkeypatch.setattr(
             workflow_medusa_preingest.os.path,
             "isfile",
-            lambda path: path == "./some/file.txt"
+            lambda path: path == "./some/file.txt",
         )
         results = workflow.sort_item_data(data)
         assert results == {
@@ -66,37 +64,31 @@ class TestMedusaPreingestCuration:
             initial_results,
             additional_data={
                 "files": ["somefile.txt"],
-                "directories": ["somedir"]
+                "directories": ["somedir"],
             },
-            **default_args
+            **default_args,
         )
 
         assert all(
-            new_task in new_tasks for new_task in [
-                {
-                    "type": "file",
-                    "path": "somefile.txt"
-                },
-                {
-                    "type": "directory",
-                    "path": "somedir"
-                }
+            new_task in new_tasks
+            for new_task in [
+                {"type": "file", "path": "somefile.txt"},
+                {"type": "directory", "path": "somedir"},
             ]
         )
 
     def test_get_additional_info_opens_dialog(
-            self,
-            workflow,
-            default_args,
+        self,
+        workflow,
+        default_args,
     ):
-
         user_request_factory = Mock(spec=interaction.UserRequestFactory)
         user_request_factory.confirm_removal = MagicMock()
 
         workflow.get_additional_info(
             user_request_factory=user_request_factory,
             options=default_args,
-            pretask_results=[MagicMock()]
+            pretask_results=[MagicMock()],
         )
         assert user_request_factory.confirm_removal.called is True
 
@@ -104,20 +96,14 @@ class TestMedusaPreingestCuration:
         "job_args, expected_class",
         [
             (
-                {
-                    "type": "file",
-                    "path": "somefile"
-                },
-                filesystem_tasks.DeleteFile
+                {"type": "file", "path": "somefile"},
+                filesystem_tasks.DeleteFile,
             ),
             (
-                {
-                    "type": "directory",
-                    "path": "someDirectory"
-                },
-                filesystem_tasks.DeleteDirectory
+                {"type": "directory", "path": "someDirectory"},
+                filesystem_tasks.DeleteDirectory,
             ),
-        ]
+        ],
     )
     def test_create_new_task(self, workflow, job_args, expected_class):
         task_builder = Mock(name="task_builder")
@@ -125,40 +111,34 @@ class TestMedusaPreingestCuration:
         workflow.create_new_task(task_builder, **job_args)
 
         assert isinstance(
-            task_builder.add_subtask.call_args[0][0],
-            expected_class
+            task_builder.add_subtask.call_args[0][0], expected_class
         )
 
     def test_initial_task_adds_finding_task(self, workflow, default_args):
         task_builder = Mock(spec=speedwagon.tasks.TaskBuilder)
         workflow.initial_task(task_builder, **default_args)
 
-        assert \
-            task_builder.add_subtask.call_args[0][0].__class__.__name__ == \
-            "FindOffendingFiles"
+        assert (
+            task_builder.add_subtask.call_args[0][0].__class__.__name__
+            == "FindOffendingFiles"
+        )
 
     def test_generate_report_mentions_file_name(self, workflow, default_args):
         results = [
             Mock(
                 spec=speedwagon.tasks.Result,
                 source=filesystem_tasks.DeleteFile,
-                data="some_file"
-
+                data="some_file",
             )
         ]
-        report = workflow.generate_report(
-            results=results,
-            **default_args
-        )
+        report = workflow.generate_report(results=results, **default_args)
         assert "some_file" in report
 
 
 @pytest.fixture()
 def default_user_args():
     workflow = workflow_medusa_preingest.MedusaPreingestCuration()
-    return {
-        data.label: data.value for data in workflow.job_options()
-    }
+    return {data.label: data.value for data in workflow.job_options()}
 
 
 def test_validate_missing_values():
@@ -178,13 +158,11 @@ def test_validate_path_valid(monkeypatch):
     monkeypatch.setattr(
         workflow_medusa_preingest.os.path,
         "exists",
-        lambda path: path == supposed_to_be_real_path
+        lambda path: path == supposed_to_be_real_path,
     )
 
     workflow_medusa_preingest.validate_path_valid(
-        {
-            'Path':  supposed_to_be_real_path
-        }
+        {"Path": supposed_to_be_real_path}
     )
 
 
@@ -192,17 +170,11 @@ def test_validate_path_invalid(monkeypatch):
     invalid_path = "./some/valid/path"
 
     monkeypatch.setattr(
-        workflow_medusa_preingest.os.path,
-        "exists",
-        lambda path: False
+        workflow_medusa_preingest.os.path, "exists", lambda path: False
     )
 
     with pytest.raises(ValueError):
-        workflow_medusa_preingest.validate_path_valid(
-            {
-                'Path':  invalid_path
-            }
-        )
+        workflow_medusa_preingest.validate_path_valid({"Path": invalid_path})
 
 
 class TestFindOffendingFiles:
@@ -219,21 +191,6 @@ class TestFindOffendingFiles:
             }
         )
         assert search_path in task.task_description()
-
-    def test_locate_folders(self, monkeypatch):
-        def scandir(*args, **kwargs):
-            return [
-                Mock(path="dummy")
-            ]
-
-        monkeypatch.setattr(workflow_medusa_preingest.os, "scandir", scandir)
-        result = list(
-            workflow_medusa_preingest.FindOffendingFiles.locate_folders(
-                    starting_dir="start",
-                    recursive=False
-                )
-        )
-        assert "dummy" in result
 
     def test_work_calls_locate_results(self, monkeypatch):
         search_path = "./some/path"
@@ -268,81 +225,28 @@ class TestFindOffendingFiles:
         with pytest.raises(FileNotFoundError):
             task.locate_results()
 
-    def test_locate_results_calls_locate_folders(self, monkeypatch):
-        search_path = os.path.join("some", "path")
-        task = workflow_medusa_preingest.FindOffendingFiles(
-            **{
-                "Path": search_path,
-                "Include Subdirectories": True,
-                "Locate and delete dot underscore files": True,
-                "Locate and delete .DS_Store files": True,
-                "Locate and delete Capture One files": True,
-                "Locate and delete Thumbs.db files": True,
-            }
-        )
-        monkeypatch.setattr(
-            workflow_medusa_preingest.os.path,
-            "exists", lambda path: path == search_path
-        )
-
-        locate_folders = Mock(return_value=[os.path.join(search_path, "more")])
-        monkeypatch.setattr(task, "locate_folders", locate_folders)
-
-        locate_offending_files_and_folders = Mock(
-            return_value=[os.path.join(search_path, "more", "more.txt")]
-        )
-
-        monkeypatch.setattr(
-            task,
-            "locate_offending_files_and_folders",
-            locate_offending_files_and_folders
-        )
-
-        task.locate_results()
-        assert all(
-            [
-                locate_offending_files_and_folders.called,
-                locate_folders.called
-            ],
-        )
-
-    def test_locate_folders_recursive(self, monkeypatch):
-        def walk(top, *args, **kwargs):
-            return [
-                [top, (["dummy"]), ()]
-            ]
-
-        monkeypatch.setattr(workflow_medusa_preingest.os, "walk", walk)
-        result = list(
-            workflow_medusa_preingest.FindOffendingFiles.locate_folders(
-                    starting_dir="start",
-                    recursive=True
-                )
-        )
-        assert os.path.join("start", "dummy") in result
-
-    def test_locate_offending_subdirectories_calls_find_capture_one_data(
-            self,
-            monkeypatch
-    ):
-        search_path = "./some/path"
-        task = workflow_medusa_preingest.FindOffendingFiles(
-            **{
-                "Path": search_path,
-                "Include Subdirectories": True,
-                "Locate and delete dot underscore files": True,
-                "Locate and delete .DS_Store files": True,
-                "Locate and delete Capture One files": True,
-                "Locate and delete Thumbs.db files": True,
-            }
-        )
-
-        find_capture_one_data = MagicMock()
-        monkeypatch.setattr(workflow_medusa_preingest,
-                            "find_capture_one_data",
-                            find_capture_one_data)
-        list(task.locate_offending_subdirectories(search_path))
-        assert find_capture_one_data.called is True
+    # def test_locate_offending_subdirectories_calls_find_capture_one_data(
+    #         self,
+    #         monkeypatch
+    # ):
+    #     search_path = "./some/path"
+    #     task = workflow_medusa_preingest.FindOffendingFiles(
+    #         **{
+    #             "Path": search_path,
+    #             "Include Subdirectories": True,
+    #             "Locate and delete dot underscore files": True,
+    #             "Locate and delete .DS_Store files": True,
+    #             "Locate and delete Capture One files": True,
+    #             "Locate and delete Thumbs.db files": True,
+    #         }
+    #     )
+    #
+    #     find_capture_one_data = MagicMock()
+    #     monkeypatch.setattr(workflow_medusa_preingest,
+    #                         "find_capture_one_data",
+    #                         find_capture_one_data)
+    #     list(task.locate_offending_subdirectories(search_path))
+    #     assert find_capture_one_data.called is True
 
     @pytest.fixture()
     def offending_files(self):
@@ -352,20 +256,18 @@ class TestFindOffendingFiles:
                 name=".DS_Store",
                 path=os.path.join(search_path, ".DS_Store"),
                 is_file=Mock(return_value=True),
-                is_dir=Mock(return_value=False)
+                is_dir=Mock(return_value=False),
             )
             ds_store_file.name = ".DS_Store"
 
             dot_under_score_file = Mock(
                 path=os.path.join(search_path, "._cache"),
-                is_file=Mock(return_value=True)
+                is_file=Mock(return_value=True),
             )
             dot_under_score_file.name = "._cache"
 
-            return [
-                ds_store_file,
-                dot_under_score_file
-            ]
+            return [ds_store_file, dot_under_score_file]
+
         return _make_mock_offending_files
 
     @pytest.mark.parametrize(
@@ -377,16 +279,16 @@ class TestFindOffendingFiles:
             (True, False, "._cache", None),
             (True, True, "._cache", None),
             (False, False, None, "._cache"),
-        ]
+        ],
     )
     def test_locate_offending_files(
-            self,
-            monkeypatch,
-            offending_files,
-            underscore,
-            ds_store,
-            expected_file,
-            expected_excluded_file
+        self,
+        monkeypatch,
+        offending_files,
+        underscore,
+        ds_store,
+        expected_file,
+        expected_excluded_file,
     ):
         search_path = os.path.join(".", "some", "path")
         offending_files(search_path)
@@ -401,25 +303,49 @@ class TestFindOffendingFiles:
             }
         )
 
-        def scandir(*args, **kwargs):
-            return offending_files(search_path)
+        task.filesystem_locator_strategy.locate = Mock(
+            return_value=[i.path for i in offending_files(search_path)]
+        )
 
-        monkeypatch.setattr(workflow_medusa_preingest.os, "scandir", scandir)
         if expected_file:
-            assert \
-                os.path.join(search_path, expected_file) \
-                in list(task.locate_offending_files(search_path))
+            assert os.path.join(search_path, expected_file) in list(
+                task.locate_results()
+            )
+
         if expected_excluded_file:
-            assert \
-                os.path.join(search_path, expected_excluded_file) \
-                not in list(task.locate_offending_files(search_path))
+            assert os.path.join(
+                search_path, expected_excluded_file
+            ) not in list(task.locate_results())
+
+    def test_locate_results(self):
+        search_path = os.path.join(".", "some", "path")
+        task = workflow_medusa_preingest.FindOffendingFiles(
+            **{
+                "Path": search_path,
+                "Include Subdirectories": True,
+                "Locate and delete dot underscore files": False,
+                "Locate and delete .DS_Store files": True,
+                "Locate and delete Capture One files": True,
+                "Locate and delete Thumbs.db files": False,
+            }
+        )
+
+        task.filesystem_locator_strategy = Mock(
+            locate=Mock(
+                return_value=[
+                    os.path.join(search_path, ".DS_Store"),
+                    os.path.join(search_path, "file.txt"),
+                ]
+            )
+        )
+        assert task.locate_results() == [
+            os.path.join(search_path, ".DS_Store")
+        ]
 
 
 def test_find_capture_one_data_nothing_found(monkeypatch):
     monkeypatch.setattr(
-        workflow_medusa_preingest.os.path,
-        "exists",
-        lambda path: False
+        workflow_medusa_preingest.os.path, "exists", lambda path: False
     )
     items_found = list(
         workflow_medusa_preingest.find_capture_one_data(directory=".")
@@ -432,20 +358,16 @@ def test_find_capture_one_data_found(monkeypatch):
     monkeypatch.setattr(
         workflow_medusa_preingest.os.path,
         "exists",
-        lambda path: path == os.path.join(starting_point, "CaptureOne")
+        lambda path: path == os.path.join(starting_point, "CaptureOne"),
     )
 
     def walk(top, *args, **kwargs):
         return [
             [top, (["Cache", "Settings91"]), ([])],
-            [os.path.join(top, "Cache"), (), (["someFile"])]
+            [os.path.join(top, "Cache"), (), (["someFile"])],
         ]
 
-    monkeypatch.setattr(
-        workflow_medusa_preingest.os,
-        "walk",
-        walk
-    )
+    monkeypatch.setattr(workflow_medusa_preingest.os, "walk", walk)
     items_found = list(
         workflow_medusa_preingest.find_capture_one_data(
             directory=starting_point
@@ -457,3 +379,69 @@ def test_find_capture_one_data_found(monkeypatch):
         os.path.join(starting_point, "CaptureOne", "Cache", "someFile"),
         os.path.join(starting_point, "CaptureOne"),
     ]
+
+
+class TestFilesystemItemLocator:
+    def test_locate_contents_order(self, monkeypatch):
+        files = {
+            os.path.join("."): [
+                Mock(
+                    spec_set=os.DirEntry,
+                    path=os.path.join(".", "starting_point"),
+                    is_dir=Mock(return_value=True),
+                )
+            ],
+            os.path.join(".", "starting_point", "empty_path"): [],
+            os.path.join(".", "starting_point", "nested_path"): [
+                Mock(
+                    spec_set=os.DirEntry,
+                    path=os.path.join(
+                        ".", "starting_point", "nested_path", "file1.txt"
+                    ),
+                    is_dir=Mock(return_value=False),
+                ),
+                Mock(
+                    spec_set=os.DirEntry,
+                    path=os.path.join(
+                        ".", "starting_point", "nested_path", "file2.txt"
+                    ),
+                    is_dir=Mock(return_value=False),
+                ),
+            ],
+            os.path.join(".", "starting_point"): [
+                Mock(
+                    spec_set=os.DirEntry,
+                    path=os.path.join(".", "starting_point", "empty_path"),
+                    is_dir=Mock(return_value=True),
+                ),
+                Mock(
+                    spec_set=os.DirEntry,
+                    path=os.path.join(".", "starting_point", "dummy.txt"),
+                    is_dir=Mock(return_value=False),
+                ),
+                Mock(
+                    spec_set=os.DirEntry,
+                    path=os.path.join(".", "starting_point", "nested_path"),
+                    is_dir=Mock(return_value=True),
+                ),
+            ],
+        }
+
+        def scandir(path, **kwargs):
+            return files[path]
+
+        monkeypatch.setattr(
+            workflow_medusa_preingest.os.path,
+            "exists",
+            lambda path: path in files,
+        )
+        monkeypatch.setattr(workflow_medusa_preingest.os, "scandir", scandir)
+        locator = workflow_medusa_preingest.FilesystemItemLocator()
+        assert list(locator.locate(os.path.join(".", "starting_point"))) == [
+            "empty_path",
+            os.path.join("nested_path", "file1.txt"),
+            os.path.join("nested_path", "file2.txt"),
+            "nested_path",
+            "dummy.txt",
+            os.path.join("."),
+        ]
